02e70936e659c787b2519accb0aef68d58996f1f
Geng-You Chen <gengyouchen@gmail.com>
2018-07-30 03:45:12 +0900

    [LeetCode 878] Update time/space complexity info

--- nth-magical-number/binary-search-solution.cpp
+++ nth-magical-number/binary-search-solution.cpp
@@ -11,21 +11,30 @@ private:
 		}
 		return first;
 	}
+	/*
+	 * time: O(log(min(A,B))), space: O(1)
+	 * see CLRS textbook 31.2: Greatest common divisor
+	 */
 	template <class T>
 	T gcd(T A, T B) {
 		if (B)
 			while ((A %= B) && (B %= A));
 		return A + B;
 	}
+	/*
+	 * time: O(log(min(A,B))), space: O(1)
+	 * see CLRS textbook 31.2: Greatest common divisor
+	 */
 	template <class T>
 	T lcm(T A, T B) {
-		return A * B / gcd(A, B);
+		return A / gcd(A, B) * B;
 	}
 public:
 	/* time: O(log(LONG_MAX)), space: O(1) */
 	int nthMagicalNumber(int N, int A, int B) {
+		long common = lcm(A, B);
 		return findFirstTrue(1L, LONG_MAX, [&](long ans) {
-			return ans / A + ans / B - ans / lcm(A, B) >= N;
+			return (ans / A + ans / B - ans / common) >= N;
 		}) % 1000000007;
 	}
 };
