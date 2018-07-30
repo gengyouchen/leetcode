300d7c9a1dd3aa0fcdaa93d8069464be2ededf9b
Geng-You Chen <gengyouchen@gmail.com>
2018-07-30 18:16:21 +0900

    [LeetCode 15] Add two pointers solution

---
+++ 3sum/two-pointers-solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+	template <class I, class F>
+	void findTwoSum(I first, I last, int target, F found) {
+		while (last - first > 1) {
+			int sum = *first + *(last - 1);
+			if (sum > target)
+				--last;
+			else if (sum < target)
+				++first;
+			else
+				found(first, --last);
+		}
+	}
+public:
+	/* time: O(n*log(n)), extra space: O(1) (i.e. does not count input & output) */
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> ans;
+		sort(nums.begin(), nums.end());
+		for (auto z = nums.begin(); z != nums.end(); ++z) {
+			if (z == nums.end() - 1 || *z != *(z + 1))
+				findTwoSum(nums.begin(), z, -*z, [&](auto x, auto y) {
+					if (ans.empty() || ans.back()[0] != *x || ans.back()[1] != *y)
+						ans.push_back({*x, *y, *z});
+				});
+		}
+		return ans;
+	}
+};
