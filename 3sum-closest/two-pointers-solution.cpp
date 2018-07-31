8712972efa1b4c2865cd7a6785e9241bd0ec3da1
Geng-You Chen <gengyouchen@gmail.com>
2018-07-31 14:41:29 +0900

    [LeetCode 16] Add time/space complexity info

--- 3sum-closest/two-pointers-solution.cpp
+++ 3sum-closest/two-pointers-solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 private:
 	template <class I, class T, class F>
-	void traceTwoSumSorted(I first, I last, T target, F trace) {
+	void sortedTwoSumStepByStep(I first, I last, T target, F trace) {
 		while (last - first > 1) {
-			T sum = *first + *(last - 1);
 			trace(first, last - 1);
+			T sum = *first + *(last - 1);
 			if (sum < target)
 				++first;
 			else
@@ -12,11 +12,12 @@ private:
 		}
 	}
 public:
+	/* time: O(n*log(n)), extra space: O(1) (i.e. does not count in input itself) */
 	int threeSumClosest(vector<int>& nums, int target) {
 		int ans, distance = INT_MAX;
 		sort(nums.begin(), nums.end());
 		for (auto z = nums.begin(); z != nums.end(); ++z)
-			traceTwoSumSorted(nums.begin(), z, target - *z, [&](auto x, auto y) {
+			sortedTwoSumStepByStep(nums.begin(), z, target - *z, [&](auto x, auto y) {
 				int curr = abs(*x + *y + *z - target);
 				if (curr < distance)
 					distance = curr, ans = *x + *y + *z;
