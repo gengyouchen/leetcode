482a0a3f5764702bcbf2ec7f34fc18785f6e6129
Geng-You Chen <gengyouchen@gmail.com>
2018-08-08 00:36:05 +0900

    [LeetCode 16] Fix: sort in O(1) auxiliary space

--- 3sum-closest/two-pointers-solution.cpp
+++ 3sum-closest/two-pointers-solution.cpp
@@ -1,27 +1,37 @@
 class Solution {
 private:
 	template <class I, class T, class F>
-	void sortedTwoSumStepByStep(I first, I last, T target, F trace) {
+	void sortedTwoSumClosest(I first, I last, T target, F found) {
+		I ans[2];
+		T d = numeric_limits<T>::max();
 		while (last - first > 1) {
-			trace(first, last - 1);
 			T sum = *first + *(last - 1);
+			if (abs(sum - target) < d)
+				d = abs(sum - target), ans[0] = first, ans[1] = last - 1;
 			if (sum < target)
 				++first;
-			else
+			else if (sum > target)
 				--last;
+			else
+				break;
 		}
+		if (d < numeric_limits<T>::max())
+			found(ans[0], ans[1]);
 	}
 public:
-	/* time: O(n^2), extra space: O(1) (i.e. does not count in input itself) */
+	/* time: O(n^2), space: O(1) auxiliary (i.e. does not count input itself) */
 	int threeSumClosest(vector<int>& nums, int target) {
-		int ans, distance = INT_MAX;
-		sort(nums.begin(), nums.end());
-		for (auto z = nums.begin(); z != nums.end(); ++z)
-			sortedTwoSumStepByStep(nums.begin(), z, target - *z, [&](auto x, auto y) {
-				int curr = abs(*x + *y + *z - target);
-				if (curr < distance)
-					distance = curr, ans = *x + *y + *z;
+		make_heap(nums.begin(), nums.end());
+		sort_heap(nums.begin(), nums.end());
+		int ans, d = INT_MAX;
+		for (auto z = nums.begin(); z != nums.end(); ++z) {
+			int complement = target - *z;
+			sortedTwoSumClosest(nums.begin(), z, complement, [&](auto x, auto y) {
+				int sum = *x + *y + *z;
+				if (abs(sum - target) < d)
+					d = abs(sum - target), ans = sum;
 			});
+		}
 		return ans;
 	}
 };
