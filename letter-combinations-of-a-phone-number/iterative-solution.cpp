87679b5d4be34337d958a68fe9fe49adf14d4cee
Geng-You Chen <gengyouchen@gmail.com>
2018-10-06 23:24:11 +0800

    [LeetCode 17] Fix indentation

--- letter-combinations-of-a-phone-number/iterative-solution.cpp
+++ letter-combinations-of-a-phone-number/iterative-solution.cpp
@@ -6,8 +6,8 @@ public:
 
 		unordered_map<char, string> digit2letters {
 			{'2', "abc"}, {'3', "def"},
-				{'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},
-				{'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"},
+			{'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},
+			{'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"},
 		};
 
 		const int n = digits.size();
