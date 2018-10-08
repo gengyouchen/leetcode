d6629966ffabf7872494e5eebffb3fdf26559d4d
Geng-You Chen <gengyouchen@gmail.com>
2018-10-09 02:47:30 +0800

    [LeetCode 92] Add list-based stack solution

---
+++ reverse-linked-list-ii/list-based-stack-solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		ListNode dummy(0);
+		auto out = &dummy;
+
+		for (int i = 0; i < m - 1 && head; ++i) {
+			auto x = head; /* x = head.top() */
+			head = head->next; /* head.pop() */
+
+			out->next = x, out = x;
+		}
+
+		if (head) {
+			auto nextOut = head;
+
+			/* move (n - m + 1) elements into buf */
+			ListNode *buf = NULL;
+			for (int i = 0; i < n - m + 1 && head; ++i) {
+				auto x = head; /* x = head.top() */
+				head = head->next; /* head.pop() */
+				x->next = buf, buf = x; /* buf.push(x) */
+			}
+
+			out->next = buf, out = nextOut;
+		}
+
+		out->next = head, out = NULL;
+		return dummy.next;
+	}
+};
