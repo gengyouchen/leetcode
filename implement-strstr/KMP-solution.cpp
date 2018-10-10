9a9127341ad90b23dfcb76cb08e7d724a99be463
Geng-You Chen <gengyouchen@gmail.com>
2018-10-10 18:47:35 +0900

    [LeetCode 28] Add KMP solution

---
+++ implement-strstr/KMP-solution.cpp
@@ -0,0 +1,69 @@
+class Solution {
+private:
+	vector<int> kmpTable(const string& s) {
+		/*
+		 *                    prefix   suffix
+		 *                    =====    =====
+		 * e.g. when R=13, s="ababa9487ababa..."
+		 *                        ^        ^
+		 *                        L=4      R=13
+		 *
+		 *                    =====    =====
+		 * Now, when R=14, s="ababa9487ababab..."
+		 *                         ^        ^
+		 *                        L+1       R=14
+		 *
+		 * Because '9' != 'b', we cannot ++L anymore
+		 * However, we do NOT need restart L from -1
+		 *
+		 *                                   ===
+		 *                                     ===
+		 * Reuse the previous R=4 result of "ababa..."
+		 *                                     ^ ^
+		 *                                     L R=4
+		 *
+		 *                        ===        ===
+		 * L can start from 2, s="ababa9487ababab..."
+		 *                           ^          ^
+		 *                          L+1         R
+		 *
+		 * Because 'b' == 'b', we can now ++L
+		 *
+		 *                    ====       ====
+		 * Now, when R=14, s="ababa9487ababab..."
+		 *                       ^          ^
+		 *                       L          R
+		 */
+		vector<int> R2L(s.size());
+		R2L[0] = -1;
+		int L = -1;
+		for (int R = 1; R < s.size(); ++R) {
+			while (L >= 0 && s[L + 1] != s[R])
+				L = R2L[L];
+			if (s[L + 1] == s[R])
+				++L;
+			R2L[R] = L;
+		}
+		return R2L;
+	}
+public:
+	/* O(len(needle) + len(haystack)) time, O(len(needle)) space */
+	int strStr(const string& haystack, const string& needle) {
+		if (needle.empty())
+			return 0;
+
+		const auto R2L = kmpTable(needle);
+		int L = -1;
+		for (int R = 0; R < haystack.size(); ++R) {
+			while (L >= 0 && needle[L + 1] != haystack[R])
+				L = R2L[L];
+			if (needle[L + 1] == haystack[R])
+				L++;
+			if (L == needle.size() - 1) {
+				return R - L; /* found */
+				L = R2L[L]; /* find next */
+			}
+		}
+		return -1; /* not found */
+	}
+};
