2084937c26fe30390dd8f029de76163fdfaa516f
Geng-You Chen <gengyouchen@gmail.com>
2018-10-15 14:01:55 +0900

    [LeetCode 37] Remove redundant data structure

--- sudoku-solver/constraint-propagation-solution.cpp
+++ sudoku-solver/constraint-propagation-solution.cpp
@@ -4,124 +4,111 @@
  *
  * For explanations, see his webpage: http://norvig.com/sudoku.html
  */
-static array<unordered_set<int>, 81> cell2units;
-static array<unordered_set<int>, 27> unit2cells;
+static array<unordered_set<int>, 81> cell2groups;
+static array<unordered_set<int>, 27> group2cells;
 static array<unordered_set<int>, 81> cell2peers;
-static bool tableInited = [&]() {  
+static bool inited = [&]() {
 	for (int cell = 0; cell < 81; ++cell) {
-		int row = cell / 9, col = cell % 9;
-		auto units = {row, 9 + col, 18 + (row / 3) * 3 + (col / 3)};
-		for (int unit : units) {
-			cell2units[cell].insert(unit);
-			unit2cells[unit].insert(cell);
+		const int row = cell / 9, col = cell % 9;
+		for (int group : {row, 9 + col, 18 + (row / 3) * 3 + (col / 3)}) {
+			cell2groups[cell].insert(group);
+			group2cells[group].insert(cell);
 		}
 	}
 	for (int cell = 0; cell < 81; ++cell)
-		for (int unit : cell2units[cell])
-			for (int peer : unit2cells[unit])
+		for (int group : cell2groups[cell])
+			for (int peer : group2cells[group])
 				if (peer != cell)
 					cell2peers[cell].insert(peer);
 	return true;
 }();
-
-class Choices {
-private:
-	bitset<9> bits;
-public:
-	Choices() : bits(0x3ff) { };
-	bool is_on(int i) const { return bits.test(i - 1); }
-	int count() const { return bits.count(); }
-	void eliminate(int i) { bits.reset(i - 1); }
-	int val() const {
-		for (int i = 1; i <= 9; ++i)
-			if (is_on(i))
-				return i;
-		return -1;
-	}
-};
-
 class Sudoku {
 private:
-	array<Choices, 81> cell2choices;
-	bool eliminate(int cell, int val) {
-		if (!cell2choices[cell].is_on(val))
-			return true; /* already eliminated */
-		cell2choices[cell].eliminate(val);
-
-		/*
-		 * (1) If a cell has only one possible value,
-		 *     then eliminate that value from the cell's peers.
-		 */
-		if (cell2choices[cell].count() == 0)
-			return false;
-		if (cell2choices[cell].count() == 1) {
-			const int decided = cell2choices[cell].val();
-			for (int peer : cell2peers[cell])
-				if (!eliminate(peer, decided))
-					return false;
-		}
-
-		/*
-		 * (2) If a unit has only one possible place for a value,
-		 *     then put the value there.
-		 */
-		for (int unit : cell2units[cell]) {
-			int nCandidate = 0, lastCandidate = -1;
-			for (int member : unit2cells[unit])
-				if (cell2choices[member].is_on(val))
-					lastCandidate = member, ++nCandidate;
-			if (nCandidate == 0)
-				return false;
-			if (nCandidate == 1 && !assign(lastCandidate, val))
-				return false;
-		}
-		return true;
-	}
+	array<bitset<9>, 81> choices;
 public:
-	bool assign(int cell, int val) {
-		for (int i = 1; i <= 9; ++i)
-			if (i != val && !eliminate(cell, i))
+	Sudoku() {
+		for (int cell = 0; cell < 81; ++cell)
+			choices[cell].set();
+	}
+	bool isSolved() const {
+		for (int cell = 0; cell < 81; ++cell)
+			if (choices[cell].count() != 1)
 				return false;
 		return true;
 	}
 	int getAnswer(int cell) const {
-		if (cell2choices[cell].count() != 1)
-			return -1;
-		return cell2choices[cell].val();
+		if (choices[cell].count() == 1)
+			for (int num = 1; num <= 9; ++num)
+				if (choices[cell].test(num - 1))
+					return num;
+		return -1;
 	}
-	bool isSolved() const {
-		for (int cell = 0; cell < 81; ++cell)
-			if (cell2choices[cell].count() != 1)
+	int findCellWithFewestChoices() const {
+		int ans = -1, fewest = INT_MAX;
+		for (int cell = 0; cell < 81; ++cell) {
+			const int nChoices = choices[cell].count();
+			if (nChoices > 1 && nChoices < fewest)
+				fewest = nChoices, ans = cell;
+		}
+		return ans;
+	}
+private:
+	bool eliminate(int cell, int num) {
+		if (!choices[cell].test(num - 1))
+			return true;
+		choices[cell].reset(num - 1);
+		/*
+		 * (1) If a cell has only one choice,
+		 *     then eliminate that choice from all its peers.
+		 */
+		if (choices[cell].none())
+			return false;
+		if (choices[cell].count() == 1) {
+			const int ans = getAnswer(cell);
+			for (int peer : cell2peers[cell])
+				if (!eliminate(peer, ans))
+					return false;
+		}
+		/*
+		 * (2) If a group has only one possible place for that number,
+		 *     then assign that number there.
+		 */
+		for (int group : cell2groups[cell]) {
+			int nPlaces = 0, lastPlace = -1;
+			for (int place : group2cells[group])
+				if (choices[place].test(num - 1))
+					lastPlace = place, ++nPlaces;
+			if (nPlaces == 0)
+				return false;
+			if (nPlaces == 1 && !assign(lastPlace, num))
+				return false;
+		}
+		return true;
+	}
+public:
+	bool assign(int cell, int num) {
+		for (int i = 1; i <= 9; ++i)
+			if (i != num && !eliminate(cell, i))
 				return false;
 		return true;
 	}
-	int findCellWithMRV() const {
+};
+class Solution {
+private:
+	bool backtrack(Sudoku& problem) {
+		if (problem.isSolved())
+			return true;
 		/*
 		 * Use MRV (Minimum Remaining Values) heuristic
 		 * to decide which cell should be filled next
 		 */
-		int bestCell = -1, nChoices = INT_MAX;
-		for (int cell = 0; cell < 81; ++cell) {
-			const int n = cell2choices[cell].count();
-			if (n > 1 && n < nChoices)
-				bestCell = cell, nChoices = n;
-		}
-		return bestCell;
-	}
-};
-
-class Solution {
-private:
-	bool solve(Sudoku& s) {
-		if (s.isSolved())
-			return true;
-		int cell = s.findCellWithMRV();
+		int cell = problem.findCellWithFewestChoices();
 		if (cell == -1)
 			return false;
-		for (int i = 1; i <= 9; i++) {
-			Sudoku tmp = s;
-			if (tmp.assign(cell, i) && solve(tmp)) {
-				s = tmp;
+		for (int num = 1; num <= 9; ++num) {
+			Sudoku tmp = problem;
+			if (tmp.assign(cell, num) && backtrack(tmp)) {
+				problem = tmp;
 				return true;
 			}
 		}
@@ -130,15 +117,14 @@ private:
 public:
 	/* time: O(9 ^ (# of undecided cells)), space: O(n) */
 	void solveSudoku(vector<vector<char>>& board) {
-		Sudoku s;
+		Sudoku problem;
 		for (int row = 0; row < 9; ++row)
 			for (int col = 0; col < 9; ++col)
 				if (isdigit(board[row][col]))
-					s.assign(row * 9 + col, board[row][col] - '0');
-		if (!solve(s))
-			return;
-		for (int row = 0; row < 9; ++row)
-			for (int col = 0; col < 9; ++col)
-				board[row][col] = s.getAnswer(row * 9 + col) + '0';
+					problem.assign(row * 9 + col, board[row][col] - '0');
+		if (backtrack(problem))
+			for (int row = 0; row < 9; ++row)
+				for (int col = 0; col < 9; ++col)
+					board[row][col] = problem.getAnswer(row * 9 + col) + '0';
 	}
 };
