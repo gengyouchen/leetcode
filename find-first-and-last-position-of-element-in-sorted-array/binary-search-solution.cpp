a9e7292bb4e0d47fe91da4dab106f7839e1183ad
Geng-You Chen <gengyouchen@gmail.com>
2018-10-18 23:29:40 +0900

    [LeetCode 34] Remove redundant condition check

--- find-first-and-last-position-of-element-in-sorted-array/binary-search-solution.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/binary-search-solution.cpp
@@ -14,14 +14,10 @@ private:
 public:
 	/* time: O(log(n)), space: O(1) */
 	vector<int> searchRange(const vector<int>& nums, int target) {
-		if (nums.empty())
-			return {-1, -1};
-
 		auto L = nums.begin(), R = nums.end();
 		L = findFirstTrue(L, R, [&](auto it) { return *it >= target; });
 		if (L == nums.end() || *L != target)
 			return {-1, -1};
-
 		R = findFirstTrue(L, R, [&](auto it) { return *it > target; });
 		return {distance(nums.begin(), L), distance(nums.begin(), R - 1)};
 	}
