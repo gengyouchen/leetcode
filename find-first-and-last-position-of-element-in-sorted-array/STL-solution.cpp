0c06984de803860cc134a5c681f9d80184b50105
Geng-You Chen <gengyouchen@gmail.com>
2018-10-18 23:50:20 +0900

    [LeetCode 34] Add STL solution

---
+++ find-first-and-last-position-of-element-in-sorted-array/STL-solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+	/* time: O(log(n)), space: O(1) */
+	vector<int> searchRange(const vector<int>& nums, int target) {
+		auto p = equal_range(nums.begin(), nums.end(), target);
+		if (p.first == nums.end() || *p.first != target)
+			return {-1, -1};
+		return {distance(nums.begin(), p.first), distance(nums.begin(), p.second - 1)};
+	}
+};
