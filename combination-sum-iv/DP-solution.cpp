12fb9b88431bce6186a695cc1ccea8b933663f53
Geng-You Chen <gengyouchen@gmail.com>
2018-10-20 01:14:55 +0900

    [LeetCode 377] Clarify the problem definition

--- combination-sum-iv/DP-solution.cpp
+++ combination-sum-iv/DP-solution.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
-	/* time: O(target * len(nums)), space: O(target) */
+	/*
+	 * time: O(target * len(nums)), space: O(target)
+	 *
+	 * Strictly speaking, this problem should not be named as "Combination" Sum
+	 * because different sequences are counted as different combinations.
+	 * We better called this problem "Permutation" Sum.
+	 *
+	 * For the real "Combination" Sum problem, see LeetCode 518 - Coin Change 2
+	 */
 	int combinationSum4(const vector<int>& nums, int target) {
 		vector<int> dp(target + 1, 0);
 		dp[0] = 1;
