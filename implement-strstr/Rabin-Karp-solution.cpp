28261a2f6faadaef9dca412d678af81640567c4a
Geng-You Chen <gengyouchen@gmail.com>
2018-10-22 05:18:14 +0900

    [LeetCode 28] Use repeated squaring for pow(x, a)

--- implement-strstr/Rabin-Karp-solution.cpp
+++ implement-strstr/Rabin-Karp-solution.cpp
@@ -5,28 +5,28 @@ public:
 		if (needle.empty())
 			return 0;
 
-		auto mod = [](int x) {
+		auto myMod = [](int x) {
 			const int p = 12421; /* any large prime is okay */
 			return (x % p + p) % p; /* we want (x % p) >= 0 */
 		};
-		auto pow = [&](int x, int a) {
-			if (a == 0)
-				return 1;
-			int ans = mod(x), i = 1;
-			while (i * 2 < a)
-				ans = mod(ans * ans), i *= 2;
-			while (i < a)
-				ans = mod(ans * x), ++i;
+		auto myPow = [&](int x, int a) {
+			int ans = 1, curr = myMod(x);
+			/* Use "Repeated Squaring" method (see CLRS textbook 31.6) */
+			for (int bit = 1; bit; bit <<= 1) {
+				if (a & bit)
+					ans = myMod(ans * curr);
+				curr = myMod(curr * curr);
+			}
 			return ans;
 		};
 		auto hash = [&](const string& s) {
 			int key = 0;
 			for (char c : s)
-				key = mod(key * 256 + c);
+				key = myMod(key * 256 + c);
 			return key;
 		};
 
-		const int n = haystack.size(), m = needle.size(), w = pow(256, m - 1);
+		const int n = haystack.size(), m = needle.size(), w = myPow(256, m - 1);
 		const int target = hash(needle);
 		int rollingHash = hash(haystack.substr(0, m));
 
@@ -36,7 +36,7 @@ public:
 					return L; /* found */
 
 			if (R != n - 1)
-				rollingHash = mod((rollingHash - haystack[L] * w) * 256 + haystack[R + 1]);
+				rollingHash = myMod((rollingHash - haystack[L] * w) * 256 + haystack[R + 1]);
 		}
 		return -1; /* not found */
 	}
