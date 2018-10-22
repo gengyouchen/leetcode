cd47cd029f49f759af6fd0dbe7ca962a447a7498
Geng-You Chen <gengyouchen@gmail.com>
2018-10-23 05:22:33 +0900

    [LeetCode 96] Update explanation of Catalan number

--- unique-binary-search-trees/DP-solution.cpp
+++ unique-binary-search-trees/DP-solution.cpp
@@ -8,9 +8,15 @@ public:
 	 * numTrees(n + 1) = sum { numTrees(i) * numTrees(n - i) for all i <= n }
 	 *
 	 * This is exactly the same recursive definition of the Catalan number
+	 * We can use DP to calculate Catalan(n) in O(n^2) time, O(n) space
+	 *
+	 * The Catalan number is also defined directly in terms of binomial coefficients:
+	 * Catalan(n) = C(2n, n) / (n + 1)
+	 *
+	 * Therefore, the ratio of Catalan(n + 1) / Catalan(n) = 2(2n + 1) / (n + 2)
+	 * We can calculate Catalan(n) sequentially in O(n) time, O(1) space
 	 */
 	int numTrees(int n) {
-		int ans = 0;
 		vector<int> Catalan(n + 1, 0);
 		Catalan[0] = 1;
 		for (int k = 0; k < n; ++k)
