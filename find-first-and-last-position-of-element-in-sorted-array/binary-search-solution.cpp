8bef936cb00812899d8da99a649a7b0c0729e903
Geng-You Chen <gengyouchen@gmail.com>
2018-11-02 06:36:53 +0900

    [LeetCode 34] Improve code readability

--- find-first-and-last-position-of-element-in-sorted-array/binary-search-solution.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/binary-search-solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 private:
-	template <class I, class F>
-	I findFirstTrue(I first, I last, F cond) {
-		while (first != last) {
-			I mid = first + (last - first) / 2;
-			if (cond(mid))
-				last = mid;
+	template <class T, class F>
+	T lowestTrue(T min, T max, F cond) {
+		while (min < max) {
+			auto it = min + (max - min) / 2;
+			if (cond(it) == true)
+				max = it;
 			else
-				first = mid + 1;
+				min = it + 1;
 		}
-		return first;
+		return min;
 	}
 public:
 	/* time: O(log(n)), space: O(1) */
 	vector<int> searchRange(const vector<int>& nums, int target) {
 		auto L = nums.begin(), R = nums.end();
-		L = findFirstTrue(L, R, [&](auto it) { return *it >= target; });
-		R = findFirstTrue(L, R, [&](auto it) { return *it > target; });
+		L = lowestTrue(L, R, [&](auto it) { return *it >= target; });
+		R = lowestTrue(L, R, [&](auto it) { return *it > target; });
 		if (L == R)
 			return {-1, -1};
 		return {distance(nums.begin(), L), distance(nums.begin(), R - 1)};
