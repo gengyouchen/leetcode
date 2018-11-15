232603e01e26c787979e9794328f9170ab294ccd
Geng-You Chen <gengyouchen@gmail.com>
2018-11-16 01:29:40 +0900

    [LeetCode 85] No need to copy histogram array

--- maximal-rectangle/reduce-to-histogram-solution.cpp
+++ maximal-rectangle/reduce-to-histogram-solution.cpp
@@ -1,27 +1,6 @@
 class Solution {
 private:
 	typedef vector<int>::iterator I;
-	/* See LeetCode 84 - Largest Rectangle in Histogram */
-	int largestRectangleArea(vector<int> heights) {
-		heights.insert(heights.begin(), -1);
-		heights.push_back(0);
-
-		int ans = 0;
-		stack<I> mono; /* Monotone Stack */
-		for (auto it = heights.begin(); it != heights.end(); ++it) {
-			while (!mono.empty() && !(*it > *mono.top())) {
-				const auto target = mono.top(), firstLessOrEqualAfterTarget = it;
-				mono.pop();
-				const auto firstLessBeforeTarget = mono.top();
-
-				const int h = *target;
-				const int w = distance(firstLessBeforeTarget, firstLessOrEqualAfterTarget) - 1;
-				ans = max(ans, w * h);
-			}
-			mono.push(it);
-		}
-		return ans;
-	}
 public:
 	/* time: O(m*n), space: O(n), where m = # of rows, n = # of cols */
 	int maximalRectangle(const vector<vector<char>>& matrix) {
@@ -29,15 +8,35 @@ public:
 			return 0;
 		const int m = matrix.size(), n = matrix[0].size();
 
+		/* See LeetCode 84 - Largest Rectangle in Histogram */
+		vector<int> heights(n + 2, 0);
+		heights[0] = -1, heights[n + 1] = 0;
+		auto largestRectangleArea = [&]() {
+			int ans = 0;
+			stack<I> mono; /* Monotone Stack */
+			for (auto it = heights.begin(); it != heights.end(); ++it) {
+				while (!mono.empty() && !(*it > *mono.top())) {
+					const auto target = mono.top(), firstLessOrEqualAfterTarget = it;
+					mono.pop();
+					const auto firstLessBeforeTarget = mono.top();
+
+					const int h = *target;
+					const int w = distance(firstLessBeforeTarget, firstLessOrEqualAfterTarget) - 1;
+					ans = max(ans, w * h);
+				}
+				mono.push(it);
+			}
+			return ans;
+		};
+
 		int ans = 0;
-		vector<int> histogram(n, 0);
 		for (int row = 0; row < m; ++row) {
 			for (int col = 0; col < n; ++col)
 				if (matrix[row][col] == '1')
-					histogram[col]++;
+					++heights[col + 1];
 				else
-					histogram[col] = 0;
-			ans = max(ans, largestRectangleArea(histogram));
+					heights[col + 1] = 0;
+			ans = max(ans, largestRectangleArea());
 		}
 		return ans;
 	}
