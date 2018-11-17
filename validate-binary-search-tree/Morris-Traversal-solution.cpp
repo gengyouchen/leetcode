83277a9e52441fa46cb5b6fe7cde3e507a58a4c1
Geng-You Chen <gengyouchen@gmail.com>
2018-11-17 17:29:40 +0900

    [LeetCode 98] Add Morris Traversal solution

---
+++ validate-binary-search-tree/Morris-Traversal-solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	/* time: O(n) time, space: O(1) auxiliary (i.e. does not count input & output itself) */
+	bool isValidBST(TreeNode* root) {
+		bool ans = true;
+		TreeNode* prev = NULL;
+		/*
+		 * Use the "Morris Traversal" method to achieve O(1) auxiliary space
+		 * See LeetCode 94 - Binary Tree Inorder Traversal
+		 */
+		while (root) {
+			if (root->left) {
+				auto pred = root->left;
+				while (pred->right && pred->right != root)
+					pred = pred->right;
+				if (!pred->right) {
+					pred->right = root;
+					root = root->left;
+					continue;
+				}
+				pred->right = NULL;
+			}
+			ans = ans && (!prev || prev->val < root->val), prev = root;
+			root = root->right;
+		}
+		return ans;
+	}
+};
