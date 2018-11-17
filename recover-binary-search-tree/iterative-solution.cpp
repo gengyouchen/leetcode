a80ff1cdc09f8804fcbe050fa024cc6589862a22
Geng-You Chen <gengyouchen@gmail.com>
2018-11-17 18:29:38 +0900

    [LeetCode 99] Add iterative solution

---
+++ recover-binary-search-tree/iterative-solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	/* time: O(n) time, space: O(n) auxiliary (i.e. does not count input & output itself) */
+	void recoverTree(TreeNode* root) {
+		TreeNode *prev = NULL, *wrong1 = NULL, *wrong2 = NULL;
+		stack<TreeNode*> succ; /* store successors to mimic threaded binary trees */
+		while (root || !succ.empty())
+			if (root)
+				succ.push(root), root = root->left;
+			else {
+				root = succ.top(), succ.pop();
+				if (prev && prev->val >= root->val) {
+					if (!wrong1)
+						wrong1 = prev;
+					wrong2 = root;
+				}
+				prev = root, root = root->right;
+			}
+		swap(wrong1->val, wrong2->val);
+	}
+};
