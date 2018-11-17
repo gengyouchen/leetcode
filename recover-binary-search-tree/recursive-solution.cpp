a420cdbd66324f484ebb401a5422ecc659c3afb6
Geng-You Chen <gengyouchen@gmail.com>
2018-11-17 18:29:01 +0900

    [LeetCode 99] Add recursive solution

---
+++ recover-binary-search-tree/recursive-solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+	typedef function<void(TreeNode*)> F;
+public:
+	/* time: O(n) time, space: O(n) auxiliary (i.e. does not count input & output itself) */
+	void recoverTree(TreeNode* root) {
+		TreeNode *prev = NULL, *wrong1 = NULL, *wrong2 = NULL;
+		F dfs = [&](auto p) {
+			if (p) {
+				dfs(p->left);
+				if (prev && prev->val >= p->val) {
+					if (!wrong1)
+						wrong1 = prev;
+					wrong2 = p;
+				}
+				prev = p;
+				dfs(p->right);
+			}
+		};
+		dfs(root);
+		swap(wrong1->val, wrong2->val);
+	}
+};
