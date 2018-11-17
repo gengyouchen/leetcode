a4af9b62da2c3d0fcd7609c63fde963b639f6efd
Geng-You Chen <gengyouchen@gmail.com>
2018-11-17 17:28:48 +0900

    [LeetCode 98] Add iterative solution

---
+++ validate-binary-search-tree/iterative-solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	/* time: O(n) time, space: O(n) auxiliary (i.e. does not count output itself) */
+	bool isValidBST(const TreeNode* root) {
+		const TreeNode* prev = NULL;
+		stack<const TreeNode*> succ; /* store successors to mimic threaded binary trees */
+		while (root || !succ.empty())
+			if (root)
+				succ.push(root), root = root->left;
+			else {
+				root = succ.top(), succ.pop();
+				if (prev && prev->val >= root->val)
+					return false;
+				prev = root, root = root->right;
+			}
+		return true;
+	}
+};
