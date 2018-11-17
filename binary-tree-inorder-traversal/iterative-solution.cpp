dc888cccfd0ad822706971dd316839254da2a51c
Geng-You Chen <gengyouchen@gmail.com>
2018-11-17 16:26:02 +0900

    [LeetCode 94] Add iterative solution

---
+++ binary-tree-inorder-traversal/iterative-solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	/* time: O(n) time, space: O(n) auxiliary (i.e. does not count output itself) */
+	vector<int> inorderTraversal(const TreeNode* root) {
+		vector<int> ans;
+		stack<const TreeNode*> succ; /* store successors to mimic threaded binary trees */
+		while (root || !succ.empty())
+			if (root)
+				succ.push(root), root = root->left;
+			else {
+				root = succ.top(), succ.pop();
+				ans.push_back(root->val), root = root->right;
+			}
+		return ans;
+	}
+};
