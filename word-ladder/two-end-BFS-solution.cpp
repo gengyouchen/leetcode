6332d3b02ab3c997f6440b05373f9ddd758a6cf2
Geng-You Chen <gengyouchen@gmail.com>
2019-01-02 01:15:37 +0900

    [LeetCode 127] Improve code readability

--- word-ladder/two-end-BFS-solution.cpp
+++ word-ladder/two-end-BFS-solution.cpp
@@ -3,22 +3,20 @@ public:
 	/* time: O(n*L*L), space: O(n*L), where n = # of words, L = each word's size */
 	int ladderLength(const string& beginWord, const string& endWord, const vector<string>& wordList) {
 		const int n = wordList.size();
-		auto word2vertex = unordered_map<string, int>();
+		unordered_map<string, int> word2vertex;
 		for (int i = 0; i < n; ++i)
 			word2vertex[wordList[i]] = i;
 		word2vertex[beginWord] = n;
-		if (!word2vertex.count(endWord))
-			return 0;
 
 		auto getAdjList = [&](int i) {
 			vector<int> adjList;
 			auto buf = (i < n) ? wordList[i] : beginWord;
 			for (int j = 0; j < buf.size(); ++j) {
-				char self = buf[j];
+				const char self = buf[j];
 				for (int k = 0; k < 26; ++k) {
 					buf[j] = 'a' + k;
 					if (buf[j] != self) {
-						auto it = word2vertex.find(buf);
+						const auto it = word2vertex.find(buf);
 						if (it != word2vertex.end())
 							adjList.push_back(it->second);
 					}
@@ -28,28 +26,28 @@ public:
 			return adjList;
 		};
 
-		auto visited = vector<bool>(n + 1, false);
-		auto source = unordered_set<int>{n};
-		auto target = unordered_set<int>{word2vertex[endWord]};
-		visited[n] = visited[word2vertex[endWord]] = true;
+		const auto it = word2vertex.find(endWord);
+		if (it == word2vertex.end())
+			return 0;
+		const int target = it->second;
 
-		int ans = 2;
-		while (!source.empty()) {
-			auto nextSource = unordered_set<int>();
-			for (int u : source) {
+		vector<bool> discovered(n + 1, false);
+		unordered_set<int> s1{n}, s2{target};
+		discovered[n] = discovered[target] = true;
+
+		for (int len = 2; !s1.empty(); ++len) {
+			unordered_set<int> next;
+			for (int u : s1) {
 				for (int v : getAdjList(u)) {
-					if (target.count(v))
-						return ans;
-					if (!visited[v]) {
-						visited[v] = true;
-						nextSource.insert(v);
-					}
+					if (s2.count(v))
+						return len;
+					if (!discovered[v])
+						next.insert(v), discovered[v] = true;
 				}
 			}
-			source = move(nextSource);
-			if (source.size() > target.size())
-				swap(source, target);
-			++ans;
+			s1 = move(next);
+			if (s1.size() > s2.size())
+				swap(s1, s2);
 		}
 		return 0;
 	}
