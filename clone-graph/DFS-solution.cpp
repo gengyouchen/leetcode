72fe8e9761031847777bcc84906990675cd5a6ed
Geng-You Chen <gengyouchen@gmail.com>
2019-01-08 02:27:35 +0900

    [LeetCode 133] Add DFS solution

---
+++ clone-graph/DFS-solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	/* time: O(|V|+|E|), space: O(|V|) auxiliary (i.e. does not count output itself) */
+	UndirectedGraphNode *cloneGraph(const UndirectedGraphNode *node) {
+		if (!node)
+			return NULL;
+
+		stack<const UndirectedGraphNode*> S;
+		unordered_map<const UndirectedGraphNode*, UndirectedGraphNode*> in2out;
+		in2out[node] = new UndirectedGraphNode(node->label), S.push(node);
+		while (!S.empty()) {
+			auto in = S.top();
+			S.pop();
+			for (auto p : in->neighbors) {
+				if (!in2out.count(p))
+					in2out[p] = new UndirectedGraphNode(p->label), S.push(p);
+			}
+		}
+
+		for (const auto& it : in2out) {
+			auto in = it.first;
+			auto out = it.second;
+			for (auto p : in->neighbors)
+				out->neighbors.push_back(in2out[p]);
+		}
+		return in2out[node];
+	}
+};
