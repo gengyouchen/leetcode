809effa1435f983b5a0026904b7a6d2bbddbf0d8
Geng-You Chen <gengyouchen@gmail.com>
2019-01-13 06:03:52 +0900

    [LeetCode 141] Improve code readability

--- linked-list-cycle/two-pointers-solution.cpp
+++ linked-list-cycle/two-pointers-solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
 	/* time: O(n), space: O(1) */
-	bool hasCycle(const ListNode *head) {
-		if (!head || !head->next)
+	bool hasCycle(ListNode *head) {
+		if (!head)
 			return false;
-
-		const ListNode *fast = head->next, *slow = head;
-		while (fast != slow) {
+		auto fast = head, slow = head;
+		do {
 			if (!fast->next || !fast->next->next)
 				return false;
 			fast = fast->next->next, slow = slow->next;
-		}
+		} while (fast != slow);
 		return true;
 	}
 };
