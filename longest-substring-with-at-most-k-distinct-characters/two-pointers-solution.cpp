06b46fee6ef376d3f54224d638e1c332989c6386
Geng-You Chen <gengyouchen@gmail.com>
2019-01-24 00:06:37 +0900

    [LeetCode 340] Improve code readability

--- longest-substring-with-at-most-k-distinct-characters/two-pointers-solution.cpp
+++ longest-substring-with-at-most-k-distinct-characters/two-pointers-solution.cpp
@@ -6,12 +6,12 @@ public:
 		int nUnique = 0, ans = 0;
 		auto L = s.begin();
 		for (auto R = s.begin(); R != s.end(); ++R) {
-			if (++nAppeared[*R] == 1)
-				++nUnique;
-			while (nUnique > k) {
+			while (nUnique == k && nAppeared[*R] == 0) {
 				if (--nAppeared[*L++] == 0)
 					--nUnique;
 			}
+			if (++nAppeared[*R] == 1)
+				++nUnique;
 			ans = max(ans, (int)distance(L, R + 1));
 		}
 		return ans;
