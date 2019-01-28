edc4e3f2be05af58a1a7fdc3d18815a85f4c1111
Geng-You Chen <gengyouchen@gmail.com>
2019-01-28 18:29:39 +0900

    [LeetCode 173] Add Morris Traversal solution

---
+++ binary-search-tree-iterator/Morris-Traversal-solution.cpp
@@ -0,0 +1,39 @@
+class BSTIterator {
+private:
+	TreeNode *root = NULL, *ans = NULL;
+	void nextAns() {
+		/*
+		 * Use the "Morris Traversal" method to achieve O(1) auxiliary space
+		 * See LeetCode 94 - Binary Tree Inorder Traversal
+		 */
+		ans = NULL;
+		while (root && !ans) {
+			if (root->left) {
+				auto pred = root->left;
+				while (pred->right && pred->right != root)
+					pred = pred->right;
+				if (!pred->right) {
+					pred->right = root;
+					root = root->left;
+					continue;
+				}
+				pred->right = NULL;
+			}
+			ans = root;
+			root = root->right;
+		}
+	}
+public:
+	/* time: O(1) amortized, space: O(1) auxiliary (i.e. does not count tree itself) */
+	BSTIterator(TreeNode* root) : root(root) { nextAns(); }
+
+	/* time: O(1) amortized, space: O(1) auxiliary (i.e. does not count tree itself) */
+	int next() {
+		const int val = ans->val;
+		nextAns();
+		return val;
+	}
+
+	/* time: O(1), space: O(1) */
+	bool hasNext() { return ans; }
+};
