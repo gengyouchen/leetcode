11190956798e6d8982ce6cf78ff36a547bf7f62c
Geng-You Chen <gengyouchen@gmail.com>
2019-02-03 01:08:36 +0900

    [LeetCode 185] Fix indentation & improve sequence

--- department-top-three-salaries/two-session-variables-solution.sql
+++ department-top-three-salaries/two-session-variables-solution.sql
@@ -1,20 +1,23 @@
 SELECT
-    Department.Name AS Department,
-    Employee.Name AS Employee,
-    Salary
+	Department.Name AS Department,
+	Employee.Name AS Employee,
+	Salary
 FROM
-    Employee
-    NATURAL JOIN (
-        SELECT
-            Salary, DepartmentId,
-            @count:=IF(@prev<=>DepartmentId, @count+1, 0) AS Rank,
-            @prev:=DepartmentId
-        FROM
-            (SELECT DISTINCT Salary, DepartmentId FROM Employee) DistinctSalaries,
-            (SELECT @count:=0, @prev:=NULL) RowInfo
-        ORDER BY DepartmentId, Salary DESC
-    ) Ranks
-    JOIN Department
-        ON DepartmentId = Department.Id
-WHERE Rank < 3
+	Employee
+	NATURAL JOIN (
+		SELECT Salary, DepartmentId
+		FROM (
+			SELECT
+				Salary, DepartmentId,
+				@count:=IF(@prev<=>DepartmentId, @count+1, 0) AS Rank,
+				@prev:=DepartmentId
+			FROM
+				(SELECT DISTINCT Salary, DepartmentId FROM Employee) DistinctSalaries,
+				(SELECT @count:=0, @prev:=NULL) RowInfo
+			ORDER BY DepartmentId, Salary DESC
+		) Ranks
+		WHERE Rank < 3
+	) GoodRanks
+	JOIN Department
+		ON DepartmentId = Department.Id
 ORDER BY DepartmentId, Salary DESC;
