d4a7270a552d9852b53e820a8d55fc6676eeefcc
Geng-You Chen <gengyouchen@gmail.com>
2019-02-10 10:33:50 +0900

    [LeetCode 28] Improve code readability

--- implement-strstr/KMP-solution.cpp
+++ implement-strstr/KMP-solution.cpp
@@ -2,66 +2,69 @@ class Solution {
 private:
 	vector<int> kmpTable(const string& s) {
 		/*
+		 * How to generate the KMP table in O(n) time?
+		 *
 		 *                    prefix   suffix
 		 *                    =====    =====
-		 * e.g. when R=13, s="ababa9487ababa..."
+		 * e.g. when i=13, s="ababa9487ababa...", ans=5 (matched length)
 		 *                        ^        ^
-		 *                        L=4      R=13
+		 *                                 i=13
 		 *
 		 *                    =====    =====
-		 * Now, when R=14, s="ababa9487ababab..."
+		 * Now, when i=14, s="ababa9487ababab..."
 		 *                         ^        ^
-		 *                        L+1       R=14
+		 *                                  i=14
 		 *
-		 * Because '9' != 'b', we cannot ++L anymore
-		 * However, we do NOT need to restart L=-1
+		 * Because '9' != 'b', we cannot ++ans anymore
+		 * However, we do NOT need to restart ans=0
 		 *
 		 *                                   ===
 		 *                                     ===
-		 * Reuse the previous R=4 result of "ababa..."
+		 * Reuse the previous i=4 result of "ababa...", ans=3 (matched length)
 		 *                                     ^ ^
-		 *                                     L R=4
+		 *                                       i=4
 		 *
-		 *                        ===        ===
-		 * We can restart L=2, s="ababa9487ababab..."
-		 *                           ^          ^
-		 *                          L+1         R
+		 *                               ===        ===
+		 * We can restart from ans=3, s="ababa9487ababab..."
+		 *                                  ^          ^
+		 *                                             i=14
 		 *
-		 * Because 'b' == 'b', we can now ++L
+		 * Because 'b' == 'b', we can now ++ans
 		 *
 		 *                    ====       ====
-		 * Now, when R=14, s="ababa9487ababab..."
-		 *                       ^          ^
-		 *                       L          R
+		 * Now, when i=15, s="ababa9487ababab...", ans=4 (matched length)
+		 *                                  ^
+		 *                                  i=15
 		 */
-		vector<int> R2L(s.size());
-		R2L[0] = -1;
-		int L = -1;
-		for (int R = 1; R < s.size(); ++R) {
-			while (L >= 0 && s[L + 1] != s[R])
-				L = R2L[L];
-			if (s[L + 1] == s[R])
-				++L;
-			R2L[R] = L;
+		const int n = s.size();
+		vector<int> dp(n);
+		dp[0] = 0;
+		int ans = 0;
+		for (int i = 1; i < n; ++i) {
+			while (ans > 0 && s[ans] != s[i])
+				ans = dp[ans - 1];
+			if (s[ans] == s[i])
+				++ans;
+			dp[i] = ans;
 		}
-		return R2L;
+		return dp;
 	}
 public:
-	/* O(len(needle) + len(haystack)) time, O(len(needle)) space */
+	/* time: O(m+n), space: O(n), where m=len(haystack), n=len(needle) */
 	int strStr(const string& haystack, const string& needle) {
 		if (needle.empty())
 			return 0;
-
-		const auto R2L = kmpTable(needle);
-		int L = -1;
-		for (int R = 0; R < haystack.size(); ++R) {
-			while (L >= 0 && needle[L + 1] != haystack[R])
-				L = R2L[L];
-			if (needle[L + 1] == haystack[R])
-				L++;
-			if (L == needle.size() - 1) {
-				return R - L; /* found */
-				L = R2L[L]; /* find next */
+		const int m = haystack.size(), n = needle.size();
+		const auto LPS = kmpTable(needle);
+		int nMatched = 0;
+		for (int i = 0; i < m; ++i) {
+			while (nMatched > 0 && needle[nMatched] != haystack[i])
+				nMatched = LPS[nMatched - 1];
+			if (needle[nMatched] == haystack[i])
+				++nMatched;
+			if (nMatched == n) {
+				return i - nMatched + 1; /* found */
+				nMatched = LPS[nMatched - 1]; /* find next */
 			}
 		}
 		return -1; /* not found */
