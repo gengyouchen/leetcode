7656f46552175c353447939718bab41fbf1aedc1
Geng-You Chen <gengyouchen@gmail.com>
2019-03-10 02:34:12 +0900

    [LeetCode 225] Modify comments

--- implement-stack-using-queues/queue-of-int-solution.cpp
+++ implement-stack-using-queues/queue-of-int-solution.cpp
@@ -2,26 +2,29 @@ class MyStack {
 private:
 	queue<int> q;
 public:
-	/* time: O(1), space: O(1) */
+	/* time: O(1), space: O(1) auxiliary (i.e. does not count queue capacity itself) */
 	MyStack() { }
 
-	/* time: O(n), space: O(1) auxiliary (i.e. does not count queue capacity itself) */
+	/*
+	 * time: O(n), space: O(1) auxiliary (i.e. does not count queue capacity itself),
+	 * where n = # of elements in queue
+	 */
 	void push(int x) {
 		q.push(x);
 		for (int i = q.size() - 1; i > 0; --i)
 			q.push(q.front()), q.pop();
 	}
 
-	/* time: O(1), space: O(1) */
+	/* time: O(1), space: O(1) auxiliary (i.e. does not count queue capacity itself) */
 	int pop() {
 		int x = q.front();
 		q.pop();
 		return x;
 	}
 
-	/* time: O(1), space: O(1) */
+	/* time: O(1), space: O(1) auxiliary (i.e. does not count queue capacity itself) */
 	int top() const { return q.front(); }
 
-	/* time: O(1), space: O(1) */
+	/* time: O(1), space: O(1) auxiliary (i.e. does not count queue capacity itself) */
 	bool empty() const { return q.empty(); }
 };
