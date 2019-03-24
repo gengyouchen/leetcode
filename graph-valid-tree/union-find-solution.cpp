2af69319678567ce626a3eaa9dd9b39f73860892
Geng-You Chen <gengyouchen@gmail.com>
2019-03-25 05:45:29 +0900

    [LeetCode 261] Add union-find solution

---
+++ graph-valid-tree/union-find-solution.cpp
@@ -0,0 +1,44 @@
+class DisjointSet {
+private:
+	vector<int> p, rank;
+	int nSet;
+public:
+	DisjointSet(int n) : p(n), rank(n, 0), nSet(n) {
+		iota(p.begin(), p.end(), 0);
+	}
+	int FindSet(int x) {
+		if (p[x] != x)
+			p[x] = FindSet(p[x]);
+		return p[x];
+	}
+	void Union(int x, int y) {
+		x = FindSet(x), y = FindSet(y);
+		if (x != y) {
+			--nSet;
+			if (rank[x] > rank[y]) {
+				p[y] = x;
+			} else {
+				p[x] = y;
+				if (rank[x] == rank[y])
+					++rank[y];
+			}
+		}
+	}
+	int CountSet() const {
+		return nSet;
+	};
+};
+
+class Solution {
+public:
+	bool validTree(int n, const vector<pair<int, int>>& edges) {
+		DisjointSet s(n);
+		for (const auto& edge : edges) {
+			const int u = edge.first, v = edge.second;
+			if (s.FindSet(u) == s.FindSet(v))
+				return false;
+			s.Union(u, v);
+		}
+		return (s.CountSet() == 1);
+	}
+};
