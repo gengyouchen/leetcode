e5227e8ad940e4d82e21d110682bb57baa80f0a9
Geng-You Chen <gengyouchen@gmail.com>
2019-04-01 04:52:48 +0900

    [LeetCode 264] Add DP solution

---
+++ ugly-number-ii/DP-solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+	/* time: O(n), space: O(n) */
+	int nthUglyNumber(int n) {
+		/*
+		 * Use the given hint from this problem:
+		 * ugly: 1,  2,    3,  4,    5,  6,    8,  9, 10, 12
+		 *   L1: 2,  4,    6,  8,   10, 12, "16", 18, 20, 24
+		 *   L2: 3,  6,    9, 12, "15", 18,   24, 27, 30, 36
+		 *   L3: 5, 10, "15", 20,   25, 30,   40, 45, 50, 60
+		 * Therefore, ugly[11] = min(16, 15, 15) = 15
+		 *
+		 * ugly: 1,  2,  3,    4,  5,    6,    8,  9, 10, 12, 15
+		 *   L1: 2,  4,  6,    8, 10,   12, "16", 18, 20, 24, 30
+		 *   L2: 3,  6,  9,   12, 15, "18",   24, 27, 30, 36, 45
+		 *   L3: 5, 10, 15, "20", 25,   30,   40, 45, 50, 60, 75
+		 * Therefore, ugly[12] = min(16, 18, 20) = 16
+		 */
+		vector<int> ugly;
+		ugly.push_back(1);
+
+		auto L1 = [&](int k) { return ugly[k] * 2; };
+		auto L2 = [&](int k) { return ugly[k] * 3; };
+		auto L3 = [&](int k) { return ugly[k] * 5; };
+
+		int x = 0, y = 0, z = 0;
+		for (int i = 1; i < n; ++i) {
+			const int next = min(min(L1(x), L2(y)), L3(z));
+			if (L1(x) == next)
+				++x;
+			if (L2(y) == next)
+				++y;
+			if (L3(z) == next)
+				++z;
+			ugly.push_back(next);
+		}
+		return ugly.back();
+	}
+};
