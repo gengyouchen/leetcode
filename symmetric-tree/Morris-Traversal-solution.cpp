f9f91f15461d39f10d17f6e59cce28aaa0576e28
Geng-You Chen <gengyouchen@gmail.com>
2019-04-03 09:03:13 +0900

    [LeetCode 101] Improve code readability

--- symmetric-tree/Morris-Traversal-solution.cpp
+++ symmetric-tree/Morris-Traversal-solution.cpp
@@ -1,52 +1,65 @@
+class MorrisTraversal {
+private:
+	bool reversed;
+	TreeNode* &left(TreeNode *x) {
+		return reversed ? x->right : x->left;
+	}
+	TreeNode* &right(TreeNode *x) {
+		return reversed ? x->left : x->right;
+	}
+	TreeNode dummy;
+	TreeNode *curr;
+	void nextInorder() {
+		if (!curr)
+			return;
+		curr = right(curr);
+		while (curr && left(curr)) {
+			TreeNode *pred = left(curr);
+			while (right(pred) && right(pred) != curr)
+				pred = right(pred);
+			if (right(pred)) {
+				right(pred) = NULL;
+				break;
+			}
+			right(pred) = curr, curr = left(curr);
+		}
+	}
+public:
+	MorrisTraversal(TreeNode *root, bool reversed) : dummy(-1), curr(&dummy), reversed(reversed) {
+		right(&dummy) = root, nextInorder();
+	}
+	bool hasNext() const {
+		return curr;
+	}
+	TreeNode *next() {
+		TreeNode *prev = curr;
+		nextInorder();
+		return prev;
+	}
+};
+
 class Solution {
 private:
-	typedef TreeNode* T;
-	T getNext(T& root, bool reversed = false) {
-		auto L = [&](T x) -> T& { return reversed ? x->right : x->left; };
-		auto R = [&](T x) -> T& { return reversed ? x->left : x->right; };
-
-		T ans = NULL;
-		/*
-		 * Use the "Morris Traversal" method to achieve O(1) auxiliary space
-		 * See LeetCode 94 - Binary Tree Inorder Traversal
-		 */
-		while (root && !ans) {
-			if (L(root)) {
-				T pred = L(root);
-				while (R(pred) && R(pred) != root)
-					pred = R(pred);
-				if (!R(pred)) {
-					R(pred) = root;
-					root = L(root);
-					continue;
-				}
-				R(pred) = NULL;
-			}
-			ans = root;
-			root = R(root);
+	static bool isSameNode(const TreeNode *p, const TreeNode *q) {
+		return (!p && !q) || (p && q && p->val == q->val);
+	}
+	static bool isMirrowedTree(TreeNode *p, TreeNode *q) {
+		MorrisTraversal P(p, false), Q(q, true);
+		bool ans = true;
+		while (P.hasNext() && Q.hasNext()) {
+			p = P.next(), q = Q.next();
+			if (!isSameNode(p, q) || !isSameNode(p->left, q->right) || !isSameNode(p->right, q->left))
+				ans = false;
 		}
+		while (P.hasNext())
+			ans = false, p = P.next();
+		while (Q.hasNext())
+			ans = false, q = Q.next();
 		return ans;
 	}
-	T getPrevious(T& root) { return getNext(root, true); }
 public:
 	/* time: O(n), space: O(1) auxiliary (i.e. does not count input itself) */
-	bool isSymmetric(TreeNode* root) {
-		if (!root)
-			return true;
-		/* This is exactly the same problem as LeetCode 100 - Same Tree */
-		bool ans = true;
-		T p = root->left, q = root->right, i = getNext(p), j = getPrevious(q);
-		while (i && j) {
-			ans = ans &&
-				(i->val == j->val) &&
-				(i->left && j->right && i->left->val == j->right->val || !i->left && !j->right) &&
-				(i->right && j->left && i->right->val == j->left->val || !i->right && !j->left);
-			i = getNext(p), j = getPrevious(q);
-		}
-		while (i)
-			ans = false, i = getNext(p);
-		while (j)
-			ans = false, j = getPrevious(q);
-		return ans;
+	static bool isSymmetric(const TreeNode *root) {
+		return !root || isMirrowedTree(root->left, root->right);
 	}
 };
