aee228dfce730306d1987907cbf904577cd80b26
Geng-You Chen <gengyouchen@gmail.com>
2019-04-04 00:48:25 +0900

    [LeetCode 226] Improve code readability

--- invert-binary-tree/BFS-solution.cpp
+++ invert-binary-tree/BFS-solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
 	/* time: O(n), space: O(n) */
-	TreeNode* invertTree(TreeNode* root) {
-		queue<TreeNode*> q;
+	static TreeNode* invertTree(TreeNode *root) {
+		queue<TreeNode*> Q;
 		if (root)
-			q.push(root);
-		while (!q.empty()) {
-			TreeNode *p = q.front();
-			q.pop();
+			Q.push(root);
+		while (!Q.empty()) {
+			TreeNode *p = Q.front();
+			Q.pop();
 			swap(p->left, p->right);
 			if (p->left)
-				q.push(p->left);
+				Q.push(p->left);
 			if (p->right)
-				q.push(p->right);
+				Q.push(p->right);
 		}
 		return root;
 	}
