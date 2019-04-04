bbb044127a905800c2ecf243c633a8c44272a78c
Geng-You Chen <gengyouchen@gmail.com>
2019-04-04 22:48:15 +0900

    [LeetCode 273] Correct time/space complexity

--- integer-to-english-words/math-solution.cpp
+++ integer-to-english-words/math-solution.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-	/* time: O(n), space: O(1) auxiliary (i.e. does not count input & output itself) */
+	/* time: O(log(n)), space: O(1) auxiliary (i.e. does not count output itself) */
 	static string numberToWords(int num) {
 		static const unordered_map<int, string> words {
+			{0, "Zero"},
+
 			{1, "One"}, {2, "Two"}, {3, "Three"},
 			{4, "Four"}, {5, "Five"}, {6, "Six"},
 			{7, "Seven"}, {8, "Eight"}, {9, "Nine"},
@@ -13,21 +15,17 @@ public:
 
 			{11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"},
 			{14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"},
-			{17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"},
-
-			{0, "Zero"}
+			{17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"}
 		};
 
 		if (num <= 20)
 			return words.at(num);
-
 		if (num < 100) {
 			const int onePart = num % 10, tenPart = num - onePart;
 			if (!onePart)
 				return words.at(tenPart);
 			return words.at(tenPart) + ' ' + words.at(onePart);
 		}
-
 		if (num < 1000) {
 			const int nHundred = num / 100, rest = num % 100;
 			if (!rest)
@@ -35,23 +33,33 @@ public:
 			return words.at(nHundred) + " Hundred " + numberToWords(rest);
 		}
 
-		if (num < 1000000) {
-			const int nThousand = num / 1000, rest = num % 1000;
-			if (!rest)
-				return numberToWords(nThousand) + " Thousand";
-			return numberToWords(nThousand) + " Thousand " + numberToWords(rest);
+		string ans;
+		const int nBillion = num / 1000000000;
+		num %= 1000000000;
+		if (nBillion)
+			ans = numberToWords(nBillion) + " Billion";
+
+		const int nMillion = num / 1000000;
+		num %= 1000000;
+		if (nMillion) {
+			if (!ans.empty())
+				ans.push_back(' ');
+			ans += numberToWords(nMillion) + " Million";
 		}
 
-		if (num < 1000000000) {
-			const int nMillion = num / 1000000, rest = num % 1000000;
-			if (!rest)
-				return numberToWords(nMillion) + " Million";
-			return numberToWords(nMillion) + " Million " + numberToWords(rest);
+		const int nThousand = num / 1000;
+		num %= 1000;
+		if (nThousand) {
+			if (!ans.empty())
+				ans.push_back(' ');
+			ans += numberToWords(nThousand) + " Thousand";
 		}
 
-		const int nBillion = num / 1000000000, rest = num % 1000000000;
-		if (!rest)
-			return numberToWords(nBillion) + " Billion";
-		return numberToWords(nBillion) + " Billion " + numberToWords(rest);
+		if (num) {
+			if (!ans.empty())
+				ans.push_back(' ');
+			ans += numberToWords(num);
+		}
+		return ans;
 	}
 };
