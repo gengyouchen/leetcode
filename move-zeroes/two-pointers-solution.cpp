86fac2cd1f7222c675cf8229095a40b1f1258b9f
Geng-You Chen <gengyouchen@gmail.com>
2019-04-06 21:20:28 +0900

    [LeetCode 283] Correct space complexity

--- move-zeroes/two-pointers-solution.cpp
+++ move-zeroes/two-pointers-solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-	/* time: O(n), space: O(1) */
+	/* time: O(n), space: O(1) auxiliary (i.e. does not count input & output itself) */
 	static void moveZeroes(vector<int>& nums) {
 		auto L = nums.begin();
 		for (auto R = nums.begin(); R != nums.end(); ++R) {
