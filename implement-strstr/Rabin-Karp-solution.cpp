6b4326e2b9474dbf350886c32f27fb9d2623e720
Geng-You Chen <gengyouchen@gmail.com>
2019-04-08 05:08:40 +0800

    [LeetCode 28] Improve code readability

--- implement-strstr/Rabin-Karp-solution.cpp
+++ implement-strstr/Rabin-Karp-solution.cpp
@@ -1,42 +1,45 @@
+class RollingHash {
+private:
+	template <class T>
+	static T mod(T x) {
+		const T p = 1000000007, r = x % p;
+		return (r >= 0) ? r : (r + p);
+	}
+	queue<char> str;
+	stack<long long> w;
+	long long h = 0;
+public:
+	RollingHash(const string& s) {
+		for (char c : s)
+			push(c);
+	}
+	void push(char c) {
+		h = mod(h * 256 + c), w.push(w.empty() ? 1 : mod(w.top() * 256)), str.push(c);
+	}
+	void pop() {
+		h = mod(h - w.top() * str.front()), w.pop(), str.pop();
+	}
+	void pop_and_push(char c) {
+		h = mod((h - w.top() * str.front()) * 256 + c), str.pop(), str.push(c);
+	}
+	int hash() const {
+		return h;
+	}
+};
+
 class Solution {
 public:
-	/* O(len(needle) + len(haystack)) time, O(len(needle)) space */
-	int strStr(const string& haystack, const string& needle) {
-		if (needle.empty())
-			return 0;
-
-		auto myMod = [](int x) {
-			const int p = 12421; /* any large prime is okay */
-			return (x % p + p) % p; /* we want (x % p) >= 0 */
-		};
-		auto myPow = [&](int x, int a) {
-			int ans = 1, curr = myMod(x);
-			/* Use "Repeated Squaring" method (see CLRS textbook 31.6) */
-			for (int bit = 1; bit; bit <<= 1) {
-				if (a & bit)
-					ans = myMod(ans * curr);
-				curr = myMod(curr * curr);
-			}
-			return ans;
-		};
-		auto hash = [&](const string& s) {
-			int key = 0;
-			for (char c : s)
-				key = myMod(key * 256 + c);
-			return key;
-		};
-
-		const int n = haystack.size(), m = needle.size(), w = myPow(256, m - 1);
-		const int target = hash(needle);
-		int rollingHash = hash(haystack.substr(0, m));
+	/* time: O(n + m), space: O(m), where n = |haystack|, m = |needle| */
+	static int strStr(const string& haystack, const string& needle) {
+		const int n = haystack.size(), m = needle.size(), goal = RollingHash(needle).hash();
+		RollingHash curr(haystack.substr(0, m));
 
 		for (int L = 0, R = m - 1; R < n; ++L, ++R) {
-			if (rollingHash == target)
-				if (haystack.substr(L, m) == needle)
-					return L; /* found */
+			if (curr.hash() == goal && haystack.substr(L, m) == needle)
+				return L; /* found */
 
 			if (R != n - 1)
-				rollingHash = myMod((rollingHash - haystack[L] * w) * 256 + haystack[R + 1]);
+				curr.pop_and_push(haystack[R + 1]);
 		}
 		return -1; /* not found */
 	}
