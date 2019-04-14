2c813d3a7134e884bd1cc4c49b6d2b940affc024
Geng-You Chen <gengyouchen@gmail.com>
2019-04-15 06:53:32 +0800

    [LeetCode 46] Improve code readability

--- permutations/iterative-solution.cpp
+++ permutations/iterative-solution.cpp
@@ -4,37 +4,28 @@ private:
 	 * Modified from LeetCode 31 - Next Permutation:
 	 * If there's no next greater permutation, just stop.
 	 */
-	bool nextPermutation(vector<int>& nums) {
-		bool isLastPermutation = false;
-		auto it = nums.end() - 1;
-		while (it != nums.begin() && *(it - 1) >= *it)
-			--it;
+	static bool nextPermutation(vector<int>& nums) {
+		const auto curr = is_sorted_until(nums.rbegin(), nums.rend());
 		/*
-		 * Now, sub-permutation [it, it + 1, it + 2, ..., nums.end() - 1] is
-		 * the greatest permutation of itself (i.e. sorted in descending order)
+		 * The sub-permutation { *(curr-1), *(curr-2), *(curr-3), ..., *nums.rbegin() }
+		 * is already the greatest possible sub-permutation of itself
 		 */
-		if (it == nums.begin())
-			return false; /* there's no next greater permutation */
+		if (curr == nums.rend())
+			return false;
 
-		auto curr = it - 1;
-		auto next = nums.end() - 1;
-		while (*next <= *curr)
-			--next;
-		iter_swap(curr, next);
-
-		reverse(it, nums.end());
-		return true; /* succeed */
+		auto cond = [&](int num) { return num > *curr; };
+		const auto next = find_if(nums.rbegin(), nums.rend(), cond);
+		iter_swap(curr, next), reverse(nums.rbegin(), curr);
+		return true;
 	}
 public:
 	/* time: O(n * n!), space: O(1) auxiliary (i.e. does not count input & output itself) */
-	vector<vector<int>> permute(vector<int>& nums) {
-		make_heap(nums.begin(), nums.end());
-		sort_heap(nums.begin(), nums.end());
-
+	static vector<vector<int>> permute(vector<int>& nums) {
 		vector<vector<int>> ans;
-		do
+		make_heap(nums.begin(), nums.end()), sort_heap(nums.begin(), nums.end());
+		do {
 			ans.push_back(nums);
-		while (nextPermutation(nums));
+		} while (nextPermutation(nums));
 		return ans;
 	}
 };
