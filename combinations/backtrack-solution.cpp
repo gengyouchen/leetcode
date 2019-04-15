08c9a9779e40a409b2220fb2738d4c66cba77bb4
Geng-You Chen <gengyouchen@gmail.com>
2019-04-15 08:01:52 +0800

    [LeetCode 77] Add pruning

--- combinations/backtrack-solution.cpp
+++ combinations/backtrack-solution.cpp
@@ -1,22 +1,21 @@
 class Solution {
 private:
-	typedef function<void(int, int)> F;
+	typedef function<void(int)> F;
 public:
 	/* time: O(k * C(n, k)), space: O(k) auxiliary (i.e. does not count output itself) */
-	vector<vector<int>> combine(int n, int k) {
+	static vector<vector<int>> combine(int n, int k) {
 		vector<vector<int>> ans;
 		vector<int> buf;
-		F backtrack = [&](int first, int last) {
-			if (buf.size() == k)
+		F backtrack = [&](int curr) {
+			if (buf.size() == k) {
 				ans.push_back(buf);
-			else
-				for (int i = first; i != last; ++i) {
-					buf.push_back(i);
-					backtrack(i + 1, last);
-					buf.pop_back();
-				}
+			} else {
+				const int last = n - (k - buf.size() - 1); /* pruning */
+				for (int i = curr; i <= last; ++i)
+					buf.push_back(i), backtrack(i + 1), buf.pop_back();
+			}
 		};
-		backtrack(1, n + 1);
+		backtrack(1);
 		return ans;
 	}
 };
