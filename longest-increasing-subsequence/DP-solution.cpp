2e99b53c252ab89f85d30db739f0865022d50eaf
Geng-You Chen <gengyouchen@gmail.com>
2019-04-15 08:25:40 +0800

    [LeetCode 300] Add DP solution

---
+++ longest-increasing-subsequence/DP-solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	/* time: O(n^2), space: O(n) */
+	static int lengthOfLIS(const vector<int>& nums) {
+		const int n = nums.size();
+		vector<int> dp(n, 1);
+
+		int ans = 0;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < i; ++j) {
+				if (nums[j] < nums[i])
+					dp[i] = max(dp[i], dp[j] + 1);
+			}
+			ans = max(ans, dp[i]);
+		}
+		return ans;
+	}
+};
