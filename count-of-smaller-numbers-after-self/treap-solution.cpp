481028cc4d9141d23ae5c53308123b99ba6ba014
Geng-You Chen <gengyouchen@gmail.com>
2019-04-21 18:57:58 +0800

    [LeetCode 315] Add treap solution

---
+++ count-of-smaller-numbers-after-self/treap-solution.cpp
@@ -0,0 +1,72 @@
+class OrderStatisticTree {
+private:
+	struct TreeNode {
+		int key, priority = rand(), size = 1;
+		TreeNode *left = NULL, *right = NULL;
+	};
+	static int size(const TreeNode *root) { return root ? root->size : 0; };
+	static void update(TreeNode *root) {
+		root->size = 1 + size(root->left) + size(root->right);
+	}
+	static auto rotateLeft(TreeNode *root) {
+		const auto y = root->right, z = y->left;
+		y->left = root, root->right = z, update(root), update(y);
+		return y;
+	}
+	static auto rotateRight(TreeNode *root) {
+		const auto y = root->left, z = y->right;
+		y->right = root, root->left = z, update(root), update(y);
+		return y;
+	}
+	static auto insert(TreeNode *root, TreeNode *x) {
+		if (!root)
+			return x;
+		if (x->key < root->key) {
+			root->left = insert(root->left, x), update(root);
+			if (root->priority > root->left->priority)
+				return rotateRight(root);
+		} else {
+			root->right = insert(root->right, x), update(root);
+			if (root->priority > root->right->priority)
+				return rotateLeft(root);
+		}
+		return root;
+	}
+	static void erase(const TreeNode *root) {
+		if (root) {
+			erase(root->left), erase(root->right);
+			delete root;
+		}
+	}
+	TreeNode *root = NULL;
+public:
+	void insert(int key) {
+		auto x = new TreeNode();
+		x->key = key, root = insert(root, x);
+	}
+	int rank(int key) const {
+		int count = 0;
+		auto p = root;
+		while (p) {
+			if (key <= p->key)
+				p = p->left;
+			else
+				count += 1 + size(p->left), p = p->right;
+		}
+		return count;
+	}
+	~OrderStatisticTree() { erase(root); }
+};
+
+class Solution {
+public:
+	/* time: O(n*log(n)), space: O(n) */
+	static vector<int> countSmaller(const vector<int>& nums) {
+		const int n = nums.size();
+		vector<int> ans(n);
+		OrderStatisticTree ost;
+		for (int i = n - 1; i >= 0; --i)
+			ans[i] = ost.rank(nums[i]), ost.insert(nums[i]);
+		return ans;
+	}
+};
