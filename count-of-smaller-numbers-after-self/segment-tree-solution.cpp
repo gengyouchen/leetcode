5bf0f303797e71a111b8c491ef04fc792c06390b
Geng-You Chen <gengyouchen@gmail.com>
2019-04-23 01:37:25 +0800

    [LeetCode 315] Improve code readability

--- count-of-smaller-numbers-after-self/segment-tree-solution.cpp
+++ count-of-smaller-numbers-after-self/segment-tree-solution.cpp
@@ -1,44 +1,50 @@
+template <class K>
 class OrderStatisticTree {
 private:
+	unordered_map<K, int> key2leaf;
 	vector<int> segTree;
 public:
-	OrderStatisticTree(int n) {
+	template <class I>
+	OrderStatisticTree(I first, I last) {
+		vector<K> keys(first, last);
+		sort(keys.begin(), keys.end());
 		int nLeaf = 1;
-		while (nLeaf < n)
+		while (nLeaf < keys.size())
 			nLeaf *= 2;
 		segTree.resize(nLeaf * 2, 0);
+		int x = nLeaf;
+		for (const auto& key : keys)
+			key2leaf[key] = x++;
 	}
-	void insert(int x) {
-		for (int i = x + segTree.size() / 2; i > 0; i /= 2)
-			segTree[i] += 1;
+	void insert(const K& key) {
+		auto it = key2leaf.find(key);
+		assert(it != key2leaf.end());
+		for (int x = it->second; x > 0; x /= 2)
+			++segTree[x];
 	}
-	int rank(int x) const {
-		int count = 0;
-		for (int i = x + segTree.size() / 2; i > 0; i /= 2) {
-			if (i % 2)
-				count += segTree[i - 1];
+	int rank(const K& key) const {
+		int ranking = 1;
+		auto it = key2leaf.find(key);
+		assert(it != key2leaf.end());
+		for (int x = it->second; x > 1; x /= 2) {
+			if (x % 2)
+				ranking += segTree[x - 1];
 		}
-		return count;
+		return ranking;
 	}
 };
 
 class Solution {
+private:
+	typedef long long K;
 public:
 	/* time: O(n*log(n)), space: O(n) */
 	static vector<int> countSmaller(const vector<int>& nums) {
 		const int n = nums.size();
-		vector<pair<int, int>> A;
-		for (int i = 0; i < n; ++i)
-			A.emplace_back(nums[i], i);
-		sort(A.begin(), A.end());
-		for (int i = 0; i < n; ++i)
-			A[i].first = A[i].second, A[i].second = i;
-		sort(A.begin(), A.end());
-
 		vector<int> ans(n);
-		OrderStatisticTree ost(n);
+		OrderStatisticTree<K> ost(nums.begin(), nums.end());
 		for (int i = n - 1; i >= 0; --i)
-			ans[i] = ost.rank(A[i].second), ost.insert(A[i].second);
+			ans[i] = ost.rank(nums[i]) - 1, ost.insert(nums[i]);
 		return ans;
 	}
 };
