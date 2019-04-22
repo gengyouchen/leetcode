9c9828c4bd408be10d63472d8f571a6352e587a5
Geng-You Chen <gengyouchen@gmail.com>
2019-04-23 02:38:01 +0800

    [LeetCode 315] Improve code readability

--- count-of-smaller-numbers-after-self/segment-tree-solution.cpp
+++ count-of-smaller-numbers-after-self/segment-tree-solution.cpp
@@ -1,20 +1,20 @@
 template <class K>
 class OrderStatisticTree {
 private:
-	unordered_map<K, int> key2leaf;
+	map<K, int> key2leaf;
 	vector<int> segTree;
 public:
 	template <class I>
 	OrderStatisticTree(I first, I last) {
-		vector<K> keys(first, last);
-		sort(keys.begin(), keys.end());
+		for (auto it = first; it != last; ++it)
+			key2leaf.emplace(*it, 0);
 		int nLeaf = 1;
-		while (nLeaf < keys.size())
+		while (nLeaf < key2leaf.size())
 			nLeaf *= 2;
 		segTree.resize(nLeaf * 2, 0);
 		int x = nLeaf;
-		for (const auto& key : keys)
-			key2leaf[key] = x++;
+		for (auto& ii : key2leaf)
+			ii.second = x++;
 	}
 	void insert(const K& key) {
 		auto it = key2leaf.find(key);
@@ -22,13 +22,15 @@ public:
 		for (int x = it->second; x > 0; x /= 2)
 			++segTree[x];
 	}
-	int rank(const K& key) const {
+	int rank(const K& target) const {
 		int ranking = 1;
-		auto it = key2leaf.find(key);
-		assert(it != key2leaf.end());
-		for (int x = it->second; x > 1; x /= 2) {
-			if (x % 2)
-				ranking += segTree[x - 1];
+		auto it = key2leaf.lower_bound(target);
+		if (it != key2leaf.begin()) {
+			--it, ranking += segTree[it->second];
+			for (int x = it->second; x > 1; x /= 2) {
+				if (x % 2)
+					ranking += segTree[x - 1];
+			}
 		}
 		return ranking;
 	}
