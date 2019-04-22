f471298f37dd321a79b195d6df5c3b080560800c
Geng-You Chen <gengyouchen@gmail.com>
2019-04-23 00:55:43 +0800

    [LeetCode 327] Improve code readability

--- count-of-range-sum/merge-sort-solution.cpp
+++ count-of-range-sum/merge-sort-solution.cpp
@@ -1,9 +1,10 @@
 class Solution {
 private:
+	typedef long long K;
 	template <class I, class F>
 	static void mergeSort(I first, I last, F beforeMerged) {
 		if (distance(first, last) > 1) {
-			const I mid = first + distance(first, last) / 2;
+			const auto mid = first + distance(first, last) / 2;
 			mergeSort(first, mid, beforeMerged), mergeSort(mid, last, beforeMerged);
 			beforeMerged(first, mid, last), inplace_merge(first, mid, last);
 		}
@@ -12,9 +13,9 @@ public:
 	/* time: O(n*log(n)), space: O(n) */
 	static int countRangeSum(const vector<int>& nums, int lower, int upper) {
 		const int n = nums.size();
-		vector<long long> prefix(n + 1);
-		copy(nums.begin(), nums.end(), prefix.begin() + 1);
-		partial_sum(prefix.begin(), prefix.end(), prefix.begin());
+		vector<K> S(n + 1);
+		for (int i = 1; i <= n; ++i)
+			S[i] = S[i - 1] + nums[i - 1];
 
 		int ans = 0;
 		auto beforeMerged = [&](auto first, auto mid, auto last) {
@@ -27,7 +28,7 @@ public:
 				ans += distance(L0, L1);
 			}
 		};
-		mergeSort(prefix.begin(), prefix.end(), beforeMerged);
+		mergeSort(S.begin(), S.end(), beforeMerged);
 		return ans;
 	}
 };
