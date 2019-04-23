5868cbabc6b044f27ba7a790fe691e76dbb1f480
Geng-You Chen <gengyouchen@gmail.com>
2019-04-23 20:33:26 +0800

    [LeetCode 327] Improve code readability

--- count-of-range-sum/segment-tree-solution.cpp
+++ count-of-range-sum/segment-tree-solution.cpp
@@ -1,38 +1,39 @@
 template <class K>
 class OrderStatisticTree {
 private:
-	map<K, int> key2leaf;
+	vector<K> keys;
 	vector<int> segTree;
 public:
 	template <class I>
 	OrderStatisticTree(I first, I last) {
-		for (auto it = first; it != last; ++it)
-			key2leaf.emplace(*it, 0);
+		unordered_set<K> S(first, last);
+		vector<K> A(S.begin(), S.end());
+		sort(A.begin(), A.end()), keys = move(A);
 		int nLeaf = 1;
-		while (nLeaf < key2leaf.size())
+		while (nLeaf < keys.size())
 			nLeaf *= 2;
 		segTree.resize(nLeaf * 2, 0);
-		int x = nLeaf;
-		for (auto& ii : key2leaf)
-			ii.second = x++;
 	}
 	void insert(const K& key) {
-		auto it = key2leaf.find(key);
-		assert(it != key2leaf.end());
-		for (int x = it->second; x > 0; x /= 2)
-			++segTree[x];
+		const auto it = lower_bound(keys.begin(), keys.end(), key);
+		assert(it != keys.end() && *it == key);
+		int x = segTree.size() / 2 + distance(keys.begin(), it);
+		while (x > 0)
+			++segTree[x], x /= 2;
 	}
-	int rank(const K& target) const {
-		int ranking = 1;
-		auto it = key2leaf.lower_bound(target);
-		if (it != key2leaf.begin()) {
-			--it, ranking += segTree[it->second];
-			for (int x = it->second; x > 1; x /= 2) {
+	int countSmaller(const K& target) const {
+		int count = 0;
+		const auto it = lower_bound(keys.begin(), keys.end(), target);
+		if (it != keys.begin()) {
+			int x = segTree.size() / 2 + distance(keys.begin(), it - 1);
+			count += segTree[x];
+			while (x > 1) {
 				if (x % 2)
-					ranking += segTree[x - 1];
+					count += segTree[x - 1];
+				x /= 2;
 			}
 		}
-		return ranking;
+		return count;
 	}
 };
 
@@ -49,8 +50,8 @@ public:
 
 		int ans = 0;
 		OrderStatisticTree<K> ost(S.begin(), S.end());
-		for (auto s : S)
-			ans += ost.rank(s - lower + 1) - ost.rank(s - upper), ost.insert(s);
+		for (const auto& s : S)
+			ans += ost.countSmaller(s - lower + 1) - ost.countSmaller(s - upper), ost.insert(s);
 		return ans;
 	}
 };
