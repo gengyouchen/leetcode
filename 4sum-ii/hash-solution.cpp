bc00e23c7543f1afb8b3979dda18588924dcbc14
Geng-You Chen <gengyouchen@gmail.com>
2019-06-05 11:16:01 +0800

    [LeetCode 454] Improve code readability

--- 4sum-ii/hash-solution.cpp
+++ 4sum-ii/hash-solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
 	/* time: O(n^2), space: O(n^2) */
-	int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+	static int fourSumCount(const vector<int>& A, const vector<int>& B, const vector<int>& C, const vector<int>& D) {
 		unordered_map<int, int> nTwoSum;
-		for (int a : A)
+		for (int a : A) {
 			for (int b : B)
 				++nTwoSum[a + b];
+		}
 
 		int ans = 0;
-		for (int c : C)
+		for (int c : C) {
 			for (int d : D) {
-				auto complement = nTwoSum.find(-(c + d));
-				if (complement != nTwoSum.end())
-					ans += complement->second;
+				const auto it = nTwoSum.find(-(c + d));
+				if (it != nTwoSum.end())
+					ans += it->second;
 			}
+		}
 		return ans;
 	}
 };
