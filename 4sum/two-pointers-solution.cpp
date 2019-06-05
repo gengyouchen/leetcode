5c7580c1242decf91a9570ddd149d6ff3bf0872a
Geng-You Chen <gengyouchen@gmail.com>
2019-06-05 11:12:46 +0800

    [LeetCode 18] Improve code readability

--- 4sum/two-pointers-solution.cpp
+++ 4sum/two-pointers-solution.cpp
@@ -1,36 +1,35 @@
 class Solution {
-private:
-	template <class I, class T, class F>
-	void sortedTwoSum(I first, I last, T target, F found) {
-		while (last - first > 1) {
-			T sum = *first + *(last - 1);
-			if (sum < target)
-				++first;
-			else if (sum > target)
-				--last;
-			else
-				found(first, --last);
-		}
-	}
 public:
 	/* time: O(n^3), space: O(1) auxiliary (i.e. does not count input & output itself) */
-	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+	static vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		const int n = nums.size();
 		make_heap(nums.begin(), nums.end());
 		sort_heap(nums.begin(), nums.end());
+
+		auto twoSum = [&](int L, int R, int target, auto found) {
+			while (L < R) {
+				const int sum = nums[L] + nums[R];
+				if (sum < target)
+					++L;
+				else if (sum > target)
+					--R;
+				else
+					found(L, R), --R;
+			}
+		};
+
 		vector<vector<int>> ans;
-		for (auto w = nums.begin(); w != nums.end(); ++w) {
-			if (w + 1 != nums.end() && *w == *(w + 1))
-				continue; /* skip duplicates */
-			for (auto z = nums.begin(); z != w; ++z) {
-				if (z + 1 != w && *z == *(z + 1))
-					continue; /* skip duplicates */
-				int complement = target - (*z + *w);
-				sortedTwoSum(nums.begin(), z, complement, [&](auto x, auto y) {
-					vector<int> quadruplets {*x, *y, *z, *w};
-					if (!ans.empty() && equal(ans.back().begin(), ans.back().end(), quadruplets.begin()))
-						return; /* skip duplicates */
-					ans.push_back(quadruplets);
-				});
+		for (int i = 3; i < n; ++i) {
+			if (i == n - 1 || nums[i] != nums[i + 1]) {
+				for (int j = 2; j < i; ++j) {
+					if (j == i - 1 || nums[j] != nums[j + 1]) {
+						auto found = [&](int L, int R) {
+							if (ans.empty() || ans.back()[0] != nums[L] || ans.back()[1] != nums[R] || ans.back()[2] != nums[j])
+								ans.push_back({nums[L], nums[R], nums[j], nums[i]});
+						};
+						twoSum(0, j - 1, target - nums[i] - nums[j], found);
+					}
+				}
 			}
 		}
 		return ans;
