c0ae95ed7ba02b9891bec09e5a4b8395b7686592
Geng-You Chen <gengyouchen@gmail.com>
2019-06-05 08:47:02 +0800

    [LeetCode 259] Improve code readability

--- 3sum-smaller/two-pointers-solution.cpp
+++ 3sum-smaller/two-pointers-solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
-private:
-	template <class I, class T>
-	int sortedTwoSumSmaller(I first, I last, T target) {
-		int ans = 0;
-		while (last - first > 1) {
-			T sum = *first + *(last - 1);
-			if (sum < target)
-				ans += last - ++first;
-			else
-				--last;
-		}
-		return ans;
-	}
 public:
 	/* time: O(n^2), space: O(1) auxiliary (i.e. does not count input itself) */
-	int threeSumSmaller(vector<int>& nums, int target) {
+	static int threeSumSmaller(vector<int>& nums, int target) {
+		const int n = nums.size();
 		make_heap(nums.begin(), nums.end());
 		sort_heap(nums.begin(), nums.end());
+
+		auto twoSumSmaller = [&](int L, int R, int target) {
+			int count = 0;
+			while (L < R) {
+				if (nums[L] + nums[R] < target)
+					count += R - L, ++L;
+				else
+					--R;
+			}
+			return count;
+		};
+
 		int ans = 0;
-		for (auto z = nums.begin(); z != nums.end(); ++z) {
-			int complement = target - *z;
-			ans += sortedTwoSumSmaller(nums.begin(), z, complement);
-		}
+		for (int i = 2; i < n; ++i)
+			ans += twoSumSmaller(0, i - 1, target - nums[i]);
 		return ans;
 	}
 };
