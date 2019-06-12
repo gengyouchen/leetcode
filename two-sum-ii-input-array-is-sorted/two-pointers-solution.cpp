6709f563de69e73365b77efcee194e3b1df3715c
Geng-You Chen <gengyouchen@gmail.com>
2019-06-13 00:44:10 +0800

    [LeetCode 167] Improve code readability

--- two-sum-ii-input-array-is-sorted/two-pointers-solution.cpp
+++ two-sum-ii-input-array-is-sorted/two-pointers-solution.cpp
@@ -1,26 +1,19 @@
 class Solution {
-private:
-	template <class I, class T, class F>
-	void sortedTwoSum(I first, I last, T target, F found) {
-		while (last - first > 1) {
-			T sum = *first + *(last - 1);
-			if (sum < target)
-				++first;
-			else if (sum > target)
-				--last;
-			else
-				found(first, --last);
-		}
-	}
 public:
 	/* time: O(n), space: O(1) */
-	vector<int> twoSum(const vector<int>& numbers, int target) {
+	static vector<int> twoSum(const vector<int>& numbers, int target) {
+		const int n = numbers.size();
 		vector<int> ans;
-		sortedTwoSum(numbers.begin(), numbers.end(), target, [&](auto x, auto y) {
-			const int i = distance(numbers.begin(), x) + 1;
-			const int j = distance(numbers.begin(), y) + 1;
-			ans = {i, j};
-		});
-		return ans;
+		int L = 0, R = n - 1;
+		while (L < R) {
+			const int sum = numbers[L] + numbers[R];
+			if (sum < target)
+				++L;
+			else if (sum > target)
+				--R;
+			else
+				return {L + 1, R + 1};
+		}
+		return {};
 	}
 };
