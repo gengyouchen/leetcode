f796e4f9b1ced08fadbd3b5c3a80c87e003bbd48
Geng-You Chen <gengyouchen@gmail.com>
2019-06-14 16:47:46 +0800

    [LeetCode 381] Improve code readability

--- insert-delete-getrandom-o1-duplicates-allowed/two-level-hash-solution.cpp
+++ insert-delete-getrandom-o1-duplicates-allowed/two-level-hash-solution.cpp
@@ -2,14 +2,22 @@ class RandomizedCollection {
 private:
 	unordered_map<int, unordered_set<int>> num2indices;
 	vector<int> nums;
+	void swapAt(int i, int j) {
+		if (nums[i] != nums[j]) {
+			num2indices[nums[i]].erase(i), num2indices[nums[i]].insert(j);
+			num2indices[nums[j]].erase(j), num2indices[nums[j]].insert(i);
+			swap(nums[i], nums[j]);
+		}
+	}
 public:
 	/* time: O(1), space: O(1) */
 	RandomizedCollection() { srand(time(NULL)); }
 
 	/* time: O(1), space: O(1) auxiliary (i.e. does not count array and hash table itself) */
 	bool insert(int val) {
-		nums.push_back(val), num2indices[val].insert(nums.size() - 1);
-		return num2indices[val].size() == 1;
+		auto& indices = num2indices[val];
+		nums.push_back(val), indices.insert(nums.size() - 1);
+		return indices.size() == 1;
 	}
 
 	/* time: O(1), space: O(1) auxiliary (i.e. does not count array and hash table itself) */
@@ -19,13 +27,8 @@ public:
 			return false;
 		auto& indices = it->second;
 
-		const int lastVal = nums.back();
-		if (lastVal != val) {
-			const int victim = *indices.begin();
-			indices.erase(victim), nums[victim] = lastVal, num2indices[lastVal].insert(victim);
-		}
-		num2indices[lastVal].erase(nums.size() - 1), nums.pop_back();
-
+		const int victim = *indices.begin();
+		swapAt(victim, nums.size() - 1), indices.erase(nums.size() - 1), nums.pop_back();
 		if (indices.empty())
 			num2indices.erase(it);
 		return true;
