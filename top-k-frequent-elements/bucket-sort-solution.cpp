80cd21d1e62a237204dbeffa7a3bdd935caf122c
Geng-You Chen <gengyouchen@gmail.com>
2019-06-16 18:16:07 +0800

    [LeetCode 347] Improve space complexity

--- top-k-frequent-elements/bucket-sort-solution.cpp
+++ top-k-frequent-elements/bucket-sort-solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-	/* time: O(n), space: O(n) */
+	/* time: O(n), space: O(W), where W = # of unique elements */
 	static vector<int> topKFrequent(const vector<int>& nums, int k) {
 		const int n = nums.size();
 		unordered_map<int, int> num2freq;
 		for (int num : nums)
 			++num2freq[num];
 
-		vector<vector<int>> freq2nums(n + 1);
+		unordered_map<int, vector<int>> freq2nums(n + 1);
 		for (const auto& it : num2freq)
 			freq2nums[it.second].push_back(it.first);
 
 		vector<int> ans;
 		for (int freq = n; freq > 0; --freq) {
-			for (int num : freq2nums[freq]) {
-				if (ans.size() >= k)
-					return ans;
-				ans.push_back(num);
+			const auto it = freq2nums.find(freq);
+			if (it != freq2nums.end()) {
+				for (int num : it->second) {
+					if (ans.size() >= k)
+						return ans;
+					ans.push_back(num);
+				}
 			}
 		}
 		return ans;
