35c69ba7f6e03b9f115060b23c74b59561e9123e
Geng-You Chen <gengyouchen@gmail.com>
2019-07-23 06:04:05 +0800

    [LeetCode 105] Improve code readability

--- construct-binary-tree-from-preorder-and-inorder-traversal/iterative-solution.cpp
+++ construct-binary-tree-from-preorder-and-inorder-traversal/iterative-solution.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
 	/* time: O(n), space: O(n) */
-	TreeNode* buildTree(const vector<int>& preorder, const vector<int>& inorder) {
-		/*
-		 * Example 1:
-		 * preorder = [a, b, c, d, e, f, g]
-		 * inorder = [a, b, c, d, e, f, g]
-		 * answer = a-b-c-d-e-f-g
-		 *
-		 * Example 2:
-		 * preorder = [a, b, c, d, e, f, g]
-		 * inorder = [b, c, d, a, e, f, g]
-		 * answer = a-e-f-g
-		 *          |
-		 *          b-c-d
-		 */
-		stack<TreeNode*> successors;
-		TreeNode *leftParent = NULL, *ans = NULL;
-		auto discovery = preorder.begin(), finishing = inorder.begin();
-		while (finishing != inorder.end()) {
-			if (!successors.empty() && successors.top()->val == *finishing) {
-				++finishing, leftParent = successors.top(), successors.pop();
-			} else {
-				auto curr = new TreeNode(*discovery++);
-				if (leftParent)
-					leftParent->right = curr;
-				else if (!successors.empty())
-					successors.top()->left = curr;
-				else
-					ans = curr;
+	static TreeNode* buildTree(const vector<int>& preorder, const vector<int>& inorder) {
+		const int n = preorder.size();
+		TreeNode *ans = NULL;
+		stack<TreeNode*> succ;
 
-				/* move insertion position to curr->left */
-				successors.push(curr), leftParent = NULL;
-			}
+		for (int i = 0, j = 0; i < n; ++i) {
+			TreeNode *curr = new TreeNode(preorder[i]);
+			if (!ans)
+				ans = curr;
+
+			TreeNode *p = NULL;
+			while (!succ.empty() && inorder[j] == succ.top()->val)
+				p = succ.top(), succ.pop(), ++j;
+
+			if (p)
+				p->right = curr;
+			else if (!succ.empty())
+				succ.top()->left = curr;
+			succ.push(curr);
 		}
 		return ans;
 	}
