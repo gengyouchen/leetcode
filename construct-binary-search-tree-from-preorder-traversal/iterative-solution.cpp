0f9dfc086fdee1a88e63c7d77bff6ee4f8e7f87a
Geng-You Chen <gengyouchen@gmail.com>
2019-07-23 00:13:55 +0800

    [LeetCode 1008] Improve code readability

--- construct-binary-search-tree-from-preorder-traversal/iterative-solution.cpp
+++ construct-binary-search-tree-from-preorder-traversal/iterative-solution.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
 	/* time: O(n), space: O(n) */
-	TreeNode* bstFromPreorder(const vector<int>& preorder) {
-		/*
-		 * See LeetCode 105 - Construct Binary Tree from Preorder and Inorder Traversal
-		 * The method is the same. BST implicitly gives us its in-order sequence.
-		 */
-		stack<TreeNode*> successors;
-		TreeNode *leftParent = NULL, *ans = NULL;
-		auto it = preorder.begin();
-		while (it != preorder.end()) {
-			if (!successors.empty() && successors.top()->val < *it) {
-				leftParent = successors.top(), successors.pop();
-			} else {
-				auto curr = new TreeNode(*it++);
-				if (leftParent)
-					leftParent->right = curr;
-				else if (!successors.empty())
-					successors.top()->left = curr;
-				else
-					ans = curr;
+	static TreeNode* bstFromPreorder(const vector<int>& preorder) {
+		TreeNode *ans = NULL;
+		stack<TreeNode*> succ;
+		for (int val : preorder) {
+			TreeNode *curr = new TreeNode(val);
+			if (!ans)
+				ans = curr;
 
-				/* move insertion position to curr->left */
-				successors.push(curr), leftParent = NULL;
-			}
+			TreeNode *p = NULL;
+			while (!succ.empty() && val > succ.top()->val)
+				p = succ.top(), succ.pop();
+
+			if (p)
+				p->right = curr;
+			else if (!succ.empty())
+				succ.top()->left = curr;
+			succ.push(curr);
 		}
 		return ans;
 	}
