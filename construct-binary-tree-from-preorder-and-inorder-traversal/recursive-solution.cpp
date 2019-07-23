d59b4297eb37effaa02c257dab05a78a2766e905
Geng-You Chen <gengyouchen@gmail.com>
2019-07-23 19:01:37 +0800

    [LeetCode 105] Improve code readability

--- construct-binary-tree-from-preorder-and-inorder-traversal/recursive-solution.cpp
+++ construct-binary-tree-from-preorder-and-inorder-traversal/recursive-solution.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
 	/* time: O(n), space: O(n) */
 	static TreeNode* buildTree(const vector<int>& preorder, const vector<int>& inorder) {
-		const int m = preorder.size(), n = inorder.size();
+		const int n = preorder.size();
 		int i = 0, j = 0;
 
 		using F = function<TreeNode*(const TreeNode*)>;
-		F dfs = [&](auto succ) {
-			if (j == n || succ && inorder[j] == succ->val)
+		F construct = [&](auto succ) {
+			if (i == n || succ && inorder[j] == succ->val)
 				return (TreeNode *)NULL;
 
 			auto curr = new TreeNode(preorder[i++]);
-			curr->left = dfs(curr), ++j, curr->right = dfs(succ);
+			curr->left = construct(curr), ++j, curr->right = construct(succ);
 			return curr;
 		};
 
-		return dfs(NULL);
+		return construct(NULL);
 	}
 };
