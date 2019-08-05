d3249be3d70732fc3ee149c82013a67d94991ccb
Geng-You Chen <gengyouchen@gmail.com>
2019-08-06 06:44:12 +0800

    [LeetCode 248] Fix bug when 1st half num matched

--- strobogrammatic-number-iii/DP-solution.cpp
+++ strobogrammatic-number-iii/DP-solution.cpp
@@ -1,78 +1,71 @@
 class Solution {
 public:
-	/* time: O(n), space: O(n), where n = length of high number string */
-	int strobogrammaticInRange(const string& low, const string& high) {
+	/* time: O(n), space: O(n), where n = len(high) */
+	static int strobogrammaticInRange(const string& low, const string& high) {
+		if (low.size() > high.size())
+			return 0;
+		if (low.size() == high.size() && low > high)
+			return 0;
+
 		const string center = "018", MSB = "1689", other = "01689";
 		char h[256] = {};
 		h['0'] = '0', h['1'] = '1', h['6'] = '9', h['8'] = '8', h['9'] = '6';
 
 		const int n = high.size();
-		vector<long> dp(n + 1);
-		for (int i = 0; i <= n; ++i) {
-			if (i == 0)
-				dp[i] = 1;
-			else if (i == 1)
-				dp[i] = center.size();
+		vector<long long> dp(n + 1);
+		for (int len = 0; len <= n; ++len) {
+			if (len == 0)
+				dp[len] = 1;
+			else if (len == 1)
+				dp[len] = center.size();
 			else
-				dp[i] = dp[i - 2] * other.size();
+				dp[len] = dp[len - 2] * other.size();
 		}
 
-		auto countShorter = [&](const string& maxNum) -> long {
-			const int w = maxNum.size();
-			long count = 0;
-			for (int i = 1; i < w; ++i) {
-				if (i == 1)
-					count += dp[i];
-				else
-					count += dp[i] * MSB.size() / other.size();
-			}
+		auto countShorter = [&](const auto& s) {
+			long long count = 0;
+			for (int len = 0; len < s.size(); ++len)
+				count += (len <= 1) ? dp[len] : (dp[len] * MSB.size() / other.size());
 			return count;
 		};
 
-		auto countSameLength = [&](const string& maxNum) -> long {
-			const int w = maxNum.size();
-			int i = 0, j = w - 1;
-			long count = 0;
-			bool validItself = true;
-			while (i <= j) {
+		auto countSameLength = [&](const auto& s) {
+			long long count = 0;
+			int i = 0, j = s.size() - 1;
+
+			for (; i <= j; ++i, --j) {
 				const int len = j - i + 1;
 				bool hasSubProblem = false;
 				for (char c : (i == j) ? center : (i == 0) ? MSB : other) {
-					if (c < maxNum[i])
+					if (c < s[i])
 						count += (i == j) ? 1 : dp[len - 2];
-					else if (c == maxNum[i])
+					if (c == s[i])
 						hasSubProblem = true;
 				}
 				if (!hasSubProblem)
 					return count;
-
-				if (h[maxNum[i]] > maxNum[j])
-					validItself = false;
-				--j, ++i;
 			}
 
-			if (validItself)
-				++count;
-			return count;
-		};
-
-		auto isStrobogrammatic = [&](const string& num) -> bool {
-			const int w = num.size();
-			int i = 0, j = w - 1;
-			while (i <= j) {
-				if (h[num[i]] != num[j])
-					return false;
-				++i, --j;
+			for (; i < s.size(); ++i, --j) {
+				const char c = h[s[j]];
+				if (c < s[i])
+					return count + 1;
+				if (c > s[i])
+					return count;
 			}
-			return true;
+			return count + 1;
 		};
 
-		auto countLess = [&](const string& num) -> long {
-			return countShorter(num) + countSameLength(num) - (isStrobogrammatic(num) ? 1 : 0);
+		auto isStrobogrammatic = [&](const auto& s) {
+			for (int i = 0, j = s.size() - 1; i <= j; ++i, --j) {
+				if (h[s[i]] != s[j])
+					return 0; /* No */
+			}
+			return 1; /* Yes */
 		};
-		auto countLessEqual = [&](const string& num) -> long {
-			return countShorter(num) + countSameLength(num);
-		};
-		return max(countLessEqual(high) - countLess(low), (long)0);
+
+		auto countLessEqual = [&](const auto& s) { return countShorter(s) + countSameLength(s); };
+		auto countLess = [&](const auto& s) { return countLessEqual(s) - isStrobogrammatic(s); };
+		return countLessEqual(high) - countLess(low);
 	}
 };
