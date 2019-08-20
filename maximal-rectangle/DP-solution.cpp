1f2e4c63658ea2cfc5a0e978f7561f2f432e4471
Geng-You Chen <gengyouchen@gmail.com>
2019-08-20 22:21:13 +0800

    [LeetCode 85] Improve code readability

--- maximal-rectangle/DP-solution.cpp
+++ maximal-rectangle/DP-solution.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
 	/* time: O(m*n), space: O(n), where m = # of rows, n = # of cols */
-	int maximalRectangle(const vector<vector<char>>& matrix) {
-		if (matrix.empty() || matrix[0].empty())
+	static int maximalRectangle(const vector<vector<char>>& matrix) {
+		if (matrix.empty())
 			return 0;
 		const int m = matrix.size(), n = matrix[0].size();
 
-		vector<int> _height(n, 0), _firstLessBeforeTarget(n, -1), _firstLessAfterTarget(n, n);
-		auto height = [&](int row, int target) -> int& { return _height[target]; };
-		auto firstLessBeforeTarget = [&](int row, int target) -> int& { return _firstLessBeforeTarget[target]; };
-		auto firstLessAfterTarget = [&](int row, int target) -> int& { return _firstLessAfterTarget[target]; };
-
 		int ans = 0;
-		for (int row = 0; row < m; ++row) {
-			for (int target = 0; target < n; ++target)
-				if (matrix[row][target] == '1')
-					height(row, target) = height(row - 1, target) + 1;
+		vector<int> height(n), bestL(n, -1), bestR(n, n);
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (matrix[i][j] == '1')
+					++height[j];
 				else
-					height(row, target) = 0;
-
-			for (int target = 0, firstZeroBeforeTarget = -1; target < n; ++target)
-				if (matrix[row][target] == '1')
-					firstLessBeforeTarget(row, target) = max(firstLessBeforeTarget(row - 1, target), firstZeroBeforeTarget);
-				else
-					firstLessBeforeTarget(row, target) = -1, firstZeroBeforeTarget = target;
-
-			for (int target = n - 1, firstZeroAfterTarget = n; target >= 0; --target)
-				if (matrix[row][target] == '1')
-					firstLessAfterTarget(row, target) = min(firstLessAfterTarget(row - 1, target), firstZeroAfterTarget);
-				else
-					firstLessAfterTarget(row, target) = n, firstZeroAfterTarget = target;
-
-			for (int target = 0; target < n; ++target) {
-				const int h = height(row, target);
-				const int w = firstLessAfterTarget(row, target) - firstLessBeforeTarget(row, target) - 1;
-				ans = max(ans, h * w);
+					height[j] = 0 /* reset height */;
 			}
+
+			for (int j = 0, currL = -1; j < n; ++j) {
+				if (matrix[i][j] == '1')
+					bestL[j] = max(bestL[j], currL);
+				else
+					bestL[j] = -1 /* reset bestL */, currL = j;
+			}
+
+			for (int j = n - 1, currR = n; j >= 0; --j) {
+				if (matrix[i][j] == '1')
+					bestR[j] = min(bestR[j], currR);
+				else
+					bestR[j] = n /* reset bestR */, currR = j;
+			}
+
+			for (int j = 0; j < n; ++j)
+				ans = max(ans, height[j] * (bestR[j] - bestL[j] - 1));
 		}
 		return ans;
 	}
