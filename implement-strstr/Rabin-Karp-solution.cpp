04557f546238020a9f5d7447288727d956ba85f2
Geng-You Chen <gengyouchen@gmail.com>
2019-08-24 19:17:54 +0800

    [LeetCode 28] Improve code readability

--- implement-strstr/Rabin-Karp-solution.cpp
+++ implement-strstr/Rabin-Karp-solution.cpp
@@ -1,45 +1,39 @@
+#define MOD 1000000007
+
 class RollingHash {
 private:
-	template <class T>
-	static T mod(T x) {
-		const T p = 1000000007, r = x % p;
-		return (r >= 0) ? r : (r + p);
+	using LL = long long;
+	static LL mod(LL x) {
+		const LL ret = x % MOD;
+		return (ret >= 0) ? ret : (ret + MOD);
 	}
-	queue<char> str;
-	stack<long long> w;
-	long long h = 0;
+	LL h = 0, w = 0;
 public:
-	RollingHash(const string& s) {
-		for (char c : s)
-			push(c);
+	template <class I>
+	RollingHash(I first, I last) {
+		for (auto it = first; it != last; ++it)
+			h = mod(h * 256 + *it), w = w ? mod(w * 256) : 1;
 	}
-	void push(char c) {
-		h = mod(h * 256 + c), w.push(w.empty() ? 1 : mod(w.top() * 256)), str.push(c);
-	}
-	void pop() {
-		h = mod(h - w.top() * str.front()), w.pop(), str.pop();
-	}
-	void pop_and_push(char c) {
-		h = mod((h - w.top() * str.front()) * 256 + c), str.pop(), str.push(c);
-	}
-	int hash() const {
-		return h;
-	}
+	void roll(char L, char R) { h = mod((h - w * L) * 256 + R); }
+	operator int() const { return h; }
 };
 
 class Solution {
 public:
-	/* time: O(n + m), space: O(m), where n = |haystack|, m = |needle| */
+	/* time: O(n+m) expected, space: O(1), where n = |haystack|, m = |needle| */
 	static int strStr(const string& haystack, const string& needle) {
-		const int n = haystack.size(), m = needle.size(), goal = RollingHash(needle).hash();
-		RollingHash curr(haystack.substr(0, m));
+		const int m = needle.size(), n = haystack.size();
+		if (m > n)
+			return -1;
 
+		const RollingHash goal(needle.begin(), needle.end());
+		RollingHash curr(haystack.begin(), haystack.begin() + m);
 		for (int L = 0, R = m - 1; R < n; ++L, ++R) {
-			if (curr.hash() == goal && haystack.substr(L, m) == needle)
+			if (curr == goal && !haystack.compare(L, m, needle))
 				return L; /* found */
 
 			if (R != n - 1)
-				curr.pop_and_push(haystack[R + 1]);
+				curr.roll(haystack[L], haystack[R + 1]);
 		}
 		return -1; /* not found */
 	}
