4e68c40617046a341fdd987017488106ad8d250b
Geng-You Chen <gengyouchen@gmail.com>
2020-06-25 02:48:28 +0800

    [LeetCode 921] Add explanation for greedy

--- 921_minimum-add-to-make-parentheses-valid/greedy.cc
+++ 921_minimum-add-to-make-parentheses-valid/greedy.cc
@@ -1,8 +1,25 @@
 class Solution {
  public:
-  /* time: O(n), extra space: O(1) */
+  /*
+   * time: O(n), space: O(1)
+   *
+   * See also:
+   *    301. Remove Invalid Parentheses
+   *   1249. Minimum Remove to Make Valid Parentheses
+   */
   int minAddToMakeValid(const string& S) {
-    int ans = 0, n_opened = 0;
+    int ans = 0;
+
+    /*
+     * A prefix of a parentheses string could be:
+     *   1. balanced such as "(()())",
+     *   2. opened such as "(()(",
+     *   3. invalid such as "(()()))".
+     *
+     * If we want ans to be a balanced parentheses string,
+     *   every prefix of ans must be balanced or opened.
+     */
+    int n_opened = 0;
     for (char c : S) {
       switch (c) {
         case '(':
@@ -13,11 +30,17 @@ class Solution {
           if (n_opened > 0)
             --n_opened;
           else
-            ++ans; /* add '(' */
+            ++ans; /* insert '(' before c */
           break;
       }
     }
-    if (n_opened > 0) ans += n_opened; /* add ')' */
+
+    /*
+     * As long as ans is still an opened parentheses string,
+     *   appending ')' won't make any prefix of ans invalid.
+     */
+    if (n_opened > 0) ans += n_opened; /* append ')' */
+
     return ans;
   }
 };
