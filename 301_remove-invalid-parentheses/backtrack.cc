d116a3b92c40fe77798a66645fed0e2c7246f14d
Geng-You Chen <gengyouchen@gmail.com>
2020-06-26 17:24:54 +0800

    [LeetCode 301] Update related question

--- 301_remove-invalid-parentheses/backtrack.cc
+++ 301_remove-invalid-parentheses/backtrack.cc
@@ -4,15 +4,16 @@ class Solution {
    * time: O(n * 2^n), extra space: O(n) (i.e. does not count output size)
    *
    * See also:
+   *     32. Longest Valid Parentheses
    *    921. Minimum Add to Make Parentheses Valid
-   *.  1249. Minimum Remove to Make Valid Parentheses
+   *   1249. Minimum Remove to Make Valid Parentheses
    */
   vector<string> removeInvalidParentheses(const string& s) {
     const int n = s.size();
-    auto quota = minRemoveToMakeValid(s);
+    const auto quota = minRemoveToMakeValid(s);
 
     vector<string> ans;
-    vector<int> removed_l, removed_r;
+    vector<int> removed_L, removed_R;
 
     using F = function<void(int, int)>;
     F backtrack = [&](int pos, int n_opened) {
@@ -21,9 +22,9 @@ class Solution {
         string buf;
         int j = 0, k = 0;
         for (int i = 0; i < n; ++i) {
-          if (j < removed_l.size() && i == removed_l[j])
+          if (j < removed_L.size() && i == removed_L[j])
             ++j;
-          else if (k < removed_r.size() && i == removed_r[k])
+          else if (k < removed_R.size() && i == removed_R[k])
             ++k;
           else
             buf += s[i];
@@ -35,13 +36,13 @@ class Solution {
       switch (s[pos]) {
         case '(':
           /* try to remove '(' */
-          if (removed_r.size() == quota.second &&
-              removed_l.size() < quota.first) {
+          if (removed_R.size() == quota.second &&
+              removed_L.size() < quota.first) {
             if (pos == 0 || s[pos - 1] != '(' ||
-                (!removed_l.empty() && removed_l.back() == pos - 1)) {
-              removed_l.push_back(pos);
+                (!removed_L.empty() && removed_L.back() == pos - 1)) {
+              removed_L.push_back(pos);
               backtrack(pos + 1, n_opened);
-              removed_l.pop_back();
+              removed_L.pop_back();
             }
           }
 
@@ -51,12 +52,12 @@ class Solution {
 
         case ')':
           /* try to remove ')' */
-          if (removed_r.size() < quota.second) {
+          if (removed_R.size() < quota.second) {
             if (pos == 0 || s[pos - 1] != ')' ||
-                (!removed_r.empty() && removed_r.back() == pos - 1)) {
-              removed_r.push_back(pos);
+                (!removed_R.empty() && removed_R.back() == pos - 1)) {
+              removed_R.push_back(pos);
               backtrack(pos + 1, n_opened);
-              removed_r.pop_back();
+              removed_R.pop_back();
             }
           }
 
@@ -78,7 +79,7 @@ class Solution {
    * Reused the solution from 1249. Minimum Remove to Make Valid Parentheses
    */
   pair<int, int> minRemoveToMakeValid(const string& s) {
-    int n_removed_l = 0, n_removed_r = 0;
+    int n_removed_L = 0, n_removed_R = 0;
 
     /*
      * A prefix of a parentheses string could be:
@@ -100,7 +101,7 @@ class Solution {
           if (n_opened > 0)
             --n_opened;
           else
-            ++n_removed_r;
+            ++n_removed_R;
           break;
       }
     }
@@ -109,8 +110,8 @@ class Solution {
      * As long as ans is still an opened parentheses string,
      *   removing the rightmost '(' won't make any prefix of ans invalid.
      */
-    if (n_opened > 0) n_removed_l += n_opened;
+    if (n_opened > 0) n_removed_L += n_opened;
 
-    return {n_removed_l, n_removed_r};
+    return {n_removed_L, n_removed_R};
   }
 };
