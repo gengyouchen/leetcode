5aec53170a63aa400b9d2ae0bd44766ab730e8fc
Geng-You Chen <gengyouchen@gmail.com>
2020-07-05 03:53:31 +0800

    [LeetCode 22] Refine backtrack approach

--- 22_generate-parentheses/backtrack.cc
+++ 22_generate-parentheses/backtrack.cc
@@ -1,30 +1,29 @@
 class Solution {
  public:
   /*
-   * time: O(n * C(n)), where C is the Catalan number
-   * extra space: O(n) (i.e. does not count output size),
+   * time: O(n * C(n)), where C is the Catalan number,
+   *   extra space: O(n) (i.e. does not count output size)
    */
   vector<string> generateParenthesis(int n) {
     vector<string> ans;
     string buf(n * 2, ' ');
 
-    using F = function<void(int, int)>;
-    F backtrack = [&](int n_added_L, int n_added_R) {
-      const int pos = n_added_L + n_added_R;
+    auto backtrack = [&](const auto& backtrack, int n_L, int n_R) -> void {
+      int pos = n_L + n_R;
       if (pos == buf.size()) {
         ans.push_back(buf);
       } else {
-        if (n_added_L < n) {
+        if (n_L < n) {
           buf[pos] = '(';
-          backtrack(n_added_L + 1, n_added_R);
+          backtrack(backtrack, n_L + 1, n_R);
         }
-        if (n_added_R < n_added_L) {
+        if (n_R < n_L) {
           buf[pos] = ')';
-          backtrack(n_added_L, n_added_R + 1);
+          backtrack(backtrack, n_L, n_R + 1);
         }
       }
     };
-    backtrack(0, 0);
+    backtrack(backtrack, 0, 0);
     return ans;
   }
 };
