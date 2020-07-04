2ca97220cb5feb5765624e9ed318c3f9ae9d6b56
Geng-You Chen <gengyouchen@gmail.com>
2020-07-04 17:20:14 +0800

    [LeetCode 1192] Refine Tarjan BCC approach

--- 1192_critical-connections-in-a-network/Tarjan-BCC.cc
+++ 1192_critical-connections-in-a-network/Tarjan-BCC.cc
@@ -9,9 +9,9 @@ class Solution {
    */
   vector<vector<int>> criticalConnections(
       int n, const vector<vector<int>>& connections) {
-    vector<vector<int>> adj_lists(n);
-    for (const auto& connection : connections) {
-      const int u = connection[0], v = connection[1];
+    vector<list<int>> adj_lists(n);
+    for (auto& connection : connections) {
+      int u = connection[0], v = connection[1];
       adj_lists[u].push_back(v);
       adj_lists[v].push_back(u);
     }
@@ -20,23 +20,23 @@ class Solution {
     vector<int> low(n); /* for Tarjan's BCC algorithm */
     int timestamp = 0;
 
-    using F = function<void(int, int)>;
-    F dfs_visit = [&](int u, int parent) {
-      const int discovery_time = ++timestamp;
+    auto dfs = [&](const auto& dfs, int u, int predecessor) -> void {
+      int discovery_time = ++timestamp;
       low[u] = discovery_time;
 
       for (int v : adj_lists[u]) {
-        if (v == parent) continue;
+        if (v == predecessor) continue;
         if (!low[v]) {
-          dfs_visit(v, u);
-          if (discovery_time < low[v]) ans.push_back({u, v});
+          dfs(dfs, v, u);
+          if (discovery_time < low[v]) /* found a bridge */
+            ans.push_back({u, v});
         }
         low[u] = min(low[u], low[v]);
       }
     };
 
     for (int i = 0; i < n; ++i)
-      if (!low[i]) dfs_visit(i, -1);
+      if (!low[i]) dfs(dfs, i, -1);
 
     return ans;
   }
